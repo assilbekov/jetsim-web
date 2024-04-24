7e1e8756f9cf2ad1160d58266d4c6fedd60b1be0
assilbekov <askhat.assilbekov@gmail.com>
2024-04-24 09:04:55 -0300

    add client url and auth redirect configs

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,13 @@ enum backendServices {
   SIM = "sim",
 }
 
+export const clientURL =
+  process.env.NODE_ENV === "production"
+    ? "https://jetsim-web.vercel.app/"
+    : `http://localhost:${process.env.PORT}`;
+
+export const authRedirect = `${clientURL}/auth/callback`;
+
 const getBackendUrl = (service: backendServices) =>
   `https://${service}.jetsim.app/api/v1/`;
 
