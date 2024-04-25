c77fe42f343c6a9302dca697e60fefaca06e2917
assilbekov <askhat.assilbekov@gmail.com>
2024-04-25 17:27:52 -0300

    add auth layout components

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,4 +1,6 @@
+import { AuthProvider } from "@/contexts/AuthContext";
 import "./globals.css";
+import { QueryContext } from "@/contexts/QueryContext";
 
 export default function LocaleLayout({
   children,
@@ -9,7 +11,11 @@ export default function LocaleLayout({
 }) {
   return (
     <html lang={locale}>
-      <body>{children}</body>
+      <body>
+        <QueryContext>
+          <AuthProvider>{children}</AuthProvider>
+        </QueryContext>
+      </body>
     </html>
   );
 }
