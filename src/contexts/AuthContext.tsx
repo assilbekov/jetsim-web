55efb8c68d0173d3fd495de5f6b6314b49a095af
assilbekov <askhat.assilbekov@gmail.com>
2024-04-25 17:26:34 -0300

    add mock Auth context

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,94 @@
+"use client";
+
+import { getSocialAuthLink } from "@/api/auth";
+import { Tokens, getAuthSocialCallback, refreshToken } from "@/api/tokens";
+import { useRouter } from "next/navigation";
+import { ReactNode, createContext, useEffect } from "react";
+
+type AuthContextType = {
+  fetchProctedData: (
+    callback: (...args: unknown[]) => unknown
+  ) => Promise<unknown>;
+  updateTokens: (tokens: Tokens) => void;
+  socialLogin: (provider: string) => void;
+  socialAuthCallback: (provider: string) => void;
+  logout: () => void;
+};
+
+export const AuthContext = createContext<AuthContextType>({
+  fetchProctedData: async () => {},
+  updateTokens: () => {},
+  socialLogin: () => {},
+  socialAuthCallback: () => {},
+  logout: () => {},
+});
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const router = useRouter();
+
+  const socialLogin = async (provider: string) => {
+    const { link } = await getSocialAuthLink(provider);
+    router.push(link);
+  };
+
+  const socialAuthCallback = async (provider: string) => {
+    const { accessToken, refreshToken } = await getAuthSocialCallback(provider);
+
+    localStorage.setItem("accessToken", accessToken);
+    localStorage.setItem("refreshToken", refreshToken);
+
+    router.push("/en/packages");
+  };
+
+  const logout = () => {
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("refreshToken");
+    router.push("/");
+  };
+
+  const fetchProctedData = async () => {
+    const accessToken = localStorage.getItem("accessToken");
+    if (!accessToken) {
+      router.push("/");
+    }
+
+    const res = await fetch("https://sim.jetsim.app/api/v1/packages", {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    if (res.status === 401) {
+      const _refreshToken = localStorage.getItem("refreshToken");
+      if (!_refreshToken) {
+        router.push("/");
+        return;
+      }
+
+      const { accessToken } = await refreshToken(_refreshToken);
+      localStorage.setItem("accessToken", accessToken);
+      fetchProctedData();
+    }
+  };
+
+  const updateTokens = (tokens: Tokens) => {
+    localStorage.setItem("accessToken", tokens.accessToken);
+    localStorage.setItem("refreshToken", tokens.refreshToken);
+  };
+
+  useEffect(() => {}, []);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        fetchProctedData,
+        socialLogin,
+        socialAuthCallback,
+        logout,
+        updateTokens,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
