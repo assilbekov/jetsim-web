cdc9f9140ace3cead94ec20234ac16cf35fcbe92
assilbekov <askhat.assilbekov@gmail.com>
2024-05-03 09:28:59 -0300

    add logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,15 @@
 import { useTranslations } from "next-intl";
+import Image from "next/image";
 import Link from "next/link";
 
 export const Navbar = () => {
   const t = useTranslations("MainPage");
   return (
     <nav className="flex justify-between">
-      <div>Logo</div>
-      <div className="flex gap-10">
+      <div>
+        <Image src="/logo.svg" alt="logo image" width={155} height={36} />
+      </div>
+      <div className="flex gap-10 text-text-600">
         <Link href="#">{t("check_compatability")}</Link>
         <Link href="#">{t("how_to")}</Link>
         <Link href="#">{t("support")}</Link>
