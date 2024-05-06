6c62d4d76d1932c01bfea21516937a2fc3dc9e0f
assilbekov <askhat.assilbekov@gmail.com>
2024-05-06 10:46:56 -0300

    add input styles

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -37,17 +37,17 @@ export const Search = () => {
           width={24}
           height={24}
           alt="pin alt"
-          className="absolute left-9 top-[49%] translate-y-[-50%] w-5 h-5 md:w-6 md:h-6"
+          className="absolute left-6 md:left-9 top-[49%] translate-y-[-50%] w-5 h-5 md:w-6 md:h-6"
         />
         <input
-          className="py-[26px] pl-[76px] pr-[90px] w-full text-xl font-medium rounded-full border-2 shadow-[0px_4px_12px_0px_rgba(0,0,0,0.04)] border-[#EBEFF0] outline-text-600 leading-6"
+          className="py-[18px] pl-[52px] pr-[76px] md:py-[26px] md:pl-[76px] md:pr-[90px] w-full text-base md:text-xl leading-5 md:leading-6 font-medium rounded-full border-2 shadow-[0px_4px_12px_0px_rgba(0,0,0,0.04)] border-[#EBEFF0] outline-text-600"
           type="text"
           value={query}
           id="search"
           onChange={(e) => {
             setQuery(e.target.value);
           }}
-          placeholder="Search for a location"
+          placeholder="Where do you go? Text"
         />
       </div>
       <ul>
