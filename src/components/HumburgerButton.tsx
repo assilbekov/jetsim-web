32fa916d3b66fb0b58c056f835ec95802d4e247b
assilbekov <askhat.assilbekov@gmail.com>
2024-05-07 10:50:48 -0300

    stick humburger menu to the right

--- src/components/HumburgerButton.tsx
+++ src/components/HumburgerButton.tsx
@@ -21,14 +21,16 @@ export const HumburgerButton = () => {
     <button
       aria-controls="primary-navigation"
       aria-expanded="false"
-      className="mobile-nav-toggle"
+      className="mobile-nav-toggle w-12 h-10 justify-center items-center rounded-md border-2 bg-text-900 border-[#EBEFF0]"
       id="mobile-nav-toggle"
       onClick={handleButtonClick}
     >
-      <div id="humburger-icon">
-        <span></span>
-        <span></span>
-        <span></span>
+      <div className="flex justify-center items-center w-5 h-5">
+        <div id="humburger-icon">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
       </div>
     </button>
   );
