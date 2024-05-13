80bd61bf8cf44e55d148127b85fc68d07ad8fe3f
assilbekov <askhat.assilbekov@gmail.com>
2024-05-13 12:27:35 -0300

    add subheader

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 export enum HeadersVariant {
   H1 = "h1",
   H2 = "h2",
+  SUBHEADER = "subheader",
 }
 
 type HeaderProps = {
@@ -15,8 +16,17 @@ export const Header = ({ children, variant }: HeaderProps) => {
         return "text-[56px] font-medium leading-[64px] text-primary-100";
       case HeadersVariant.H2:
         return "text-[34px] font-medium leading-[38px] text-text-100";
+      case HeadersVariant.SUBHEADER:
+        return "text-[24px] font-medium leading-[28px] text-text-100";
     }
   };
 
-  return <h2 className={getVariantClassNames()}>{children}</h2>;
+  switch (variant) {
+    case HeadersVariant.H1:
+      return <h1 className={getVariantClassNames()}>{children}</h1>;
+    case HeadersVariant.H2:
+      return <h2 className={getVariantClassNames()}>{children}</h2>;
+    case HeadersVariant.SUBHEADER:
+      return <h3 className={getVariantClassNames()}>{children}</h3>;
+  }
 };
