244374b173db2fe95c22500f7e5a5ee1d306deed
assilbekov <askhat.assilbekov@gmail.com>
2024-05-13 15:45:30 -0300

    add SelectDataPlan

--- src/components/SetupJetSim.tsx
+++ src/components/SetupJetSim.tsx
@@ -4,8 +4,40 @@ import { Header, HeadersVariant } from "./Header";
 import { LandingContainer } from "./LandingContainer";
 import Link from "next/link";
 
-const Step = () => {
-  return <div>Step 1</div>;
+type SelectDataPlanProps = {
+  selected: boolean;
+  label: React.ReactNode;
+};
+
+const SelectDataPlan = ({ selected, label }: SelectDataPlanProps) => {
+  return (
+    <div className="flex gap-4">
+      <div className="flex items-center justify-center rounded-full w-6 h-6 border-2 border-[#E6F0F2]">
+        {selected && <div className="bg-secondary-500 w-2 h-2 rounded-full" />}
+      </div>
+      <p>{label}</p>
+    </div>
+  );
+};
+
+type StepProps = {
+  step: React.ReactNode;
+  title: React.ReactNode;
+  content: React.ReactNode;
+};
+
+const Step = ({ step, title, content }: StepProps) => {
+  return (
+    <div className="flex flex-col gap-6 p-8 rounded-xl bg-[#F8F9FB]">
+      <h3 className="text-[24px] font-medium leading-[28px] text-primary-500">
+        {step}
+      </h3>
+      <Header variant={HeadersVariant.SUBHEADER}>{title}</Header>
+      <div className="flex flex-col gap-6 p-6 bg-text-900 rounded-lg">
+        {content}
+      </div>
+    </div>
+  );
 };
 
 export const SetupJetSim = () => {
@@ -15,10 +47,18 @@ export const SetupJetSim = () => {
         <Header variant={HeadersVariant.H2}>
           Set up your JetSim eSIM in 1 minute
         </Header>
-        <div>
-          <Step />
-          <Step />
-          <Step />
+        <div className="flex gap-8">
+          <Step
+            step="01"
+            title="Choose destination and select data plan"
+            content={
+              <>
+                <SelectDataPlan label="5 gb" selected />
+                <SelectDataPlan label="15 gb" selected={false} />
+                <SelectDataPlan label="30 gb" selected={false} />
+              </>
+            }
+          />
         </div>
         <div className="bg-[#F8F9FB] py-6 px-8 rounded-xl relative overflow-visible">
           <div className="flex gap-8 w-3/4">
