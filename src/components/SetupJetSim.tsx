bf6370076aaeb4fcf3703b0be7eef48dc46b562d
assilbekov <askhat.assilbekov@gmail.com>
2024-05-14 08:40:11 -0300

    add DataRoaming step

--- src/components/SetupJetSim.tsx
+++ src/components/SetupJetSim.tsx
@@ -24,11 +24,44 @@ const InstructionStep = ({ step }: { step: string }) => {
   return (
     <div className="flex gap-4 items-center">
       <div className="flex items-center justify-center rounded-full min-w-6 h-6 border-2 border-[#E6F0F2]">
-        <p className="font-base leading-4 font-medium">
-          {step}
-        </p>
+        <p className="font-base leading-4 font-medium">{step}</p>
       </div>
       <div className="w-full h-3 rounded-full bg-[#E6F0F2]" />
+    </div>
+  );
+};
+
+const DisabledToggle = ({ checked }: { checked: boolean }) => {
+  return (
+    <label className="inline-flex items-center cursor-default">
+      <input
+        type="checkbox"
+        value=""
+        className="sr-only peer"
+        disabled
+        checked={checked}
+      />
+      <div className="relative w-10 h-6 bg-[rgba(120,120,128,0.16)] rounded-full peer dark:bg-text-900 peer-checked:after:translate-x-[82%] rtl:peer-checked:after:-translate-x-[82%] peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-text-900 after:border-text-900 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-text-900 peer-checked:bg-secondary-500"></div>
+    </label>
+  );
+};
+
+const DataRoaming = () => {
+  return (
+    <div className="flex gap-4 items-center">
+      <div className="w-full h-4 rounded-full bg-[#E6F0F2]" />
+      <DisabledToggle checked={false} />
+    </div>
+  );
+};
+
+const DataRoamingActive = () => {
+  return (
+    <div className="flex gap-4 items-center justify-between">
+      <p className="font-base font-medium text-text-100 leading-[22px]">
+        Data roaming
+      </p>
+      <DisabledToggle checked />
     </div>
   );
 };
@@ -39,6 +72,14 @@ type StepProps = {
   content: React.ReactNode;
 };
 
+const StepContent = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex flex-col gap-6 p-6 bg-text-900 rounded-lg">
+      {children}
+    </div>
+  );
+};
+
 const Step = ({ step, title, content }: StepProps) => {
   return (
     <div className="flex flex-col gap-6 p-8 rounded-xl bg-[#F8F9FB]">
@@ -46,9 +87,7 @@ const Step = ({ step, title, content }: StepProps) => {
         {step}
       </h3>
       <Header variant={HeadersVariant.SUBHEADER}>{title}</Header>
-      <div className="flex flex-col gap-6 p-6 bg-text-900 rounded-lg">
-        {content}
-      </div>
+      {content}
     </div>
   );
 };
@@ -65,33 +104,35 @@ export const SetupJetSim = () => {
             step="01"
             title="Choose destination and select data plan"
             content={
-              <>
+              <StepContent>
                 <SelectDataPlan label="5 gb" selected />
                 <SelectDataPlan label="15 gb" selected={false} />
                 <SelectDataPlan label="30 gb" selected={false} />
-              </>
+              </StepContent>
             }
           />
           <Step
             step="02"
             title="Follow the instruction and install eSIM"
             content={
-              <>
+              <StepContent>
                 <InstructionStep step="1" />
                 <InstructionStep step="2" />
                 <InstructionStep step="3" />
-              </>
+              </StepContent>
             }
           />
           <Step
-            step="01"
-            title="Choose destination and select data plan"
+            step="03"
+            title="Activate your data roaming on arrival"
             content={
-              <>
-                <SelectDataPlan label="5 gb" selected />
-                <SelectDataPlan label="15 gb" selected={false} />
-                <SelectDataPlan label="30 gb" selected={false} />
-              </>
+              <div className="flex flex-col gap-4 p-6 pb-1 bg-text-900 rounded-lg">
+                <DataRoamingActive />
+                <div className="h-0.5 w-full bg-[#E6F0F2]" />
+                <DataRoaming />
+                <div className="h-0.5 w-full bg-[#E6F0F2]" />
+                <DataRoaming />
+              </div>
             }
           />
         </div>
