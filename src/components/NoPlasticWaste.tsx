23fb526e74ef9c0a970b50ccfb3c688ac4f6b2b7
assilbekov <askhat.assilbekov@gmail.com>
2024-05-15 10:07:33 -0300

    add NoPlasticWaste bg gray color

--- src/components/NoPlasticWaste.tsx
+++ src/components/NoPlasticWaste.tsx
@@ -20,18 +20,20 @@ const Chip = ({ label, image }: ChipProps) => {
 export const NoPlasticWaste = () => {
   return (
     <LandingContainer>
-      <div className="w-full bg-[url('/no-waste-background.png')] bg-[bottom_50%_right_55%] bg-cover rounded-xl py-11 px-20">
-        <p className="text-text-900 font-medium text-xl leading-6 text-center">
-          No more plastic waste
-        </p>
-        <h3 className="text-text-900 text-[34px] font-medium leading-[38px] text-center mt-2">
-          Join us in our mission to save the planet
-        </h3>
-        <div className="flex gap-4 justify-center mt-8">
-          <Chip label="No plastic SIM cards" image="/no-sim.svg" />
-          <Chip label="No packaging" image="/delete.svg" />
-          <Chip label="No transport fees" image="/no_transfer.svg" />
-          <Chip label="No physical advertising" image="/scan.svg" />
+      <div className="w-full bg-[url('/no-waste-background.png')] bg-[bottom_50%_right_55%] bg-cover rounded-xl">
+        <div className="bg-gray-500 bg-opacity-50 py-11 px-20 rounded-xl">
+          <p className="text-text-900 font-medium text-xl leading-6 text-center">
+            No more plastic waste
+          </p>
+          <h3 className="text-text-900 text-[34px] font-medium leading-[38px] text-center mt-2">
+            Join us in our mission to save the planet
+          </h3>
+          <div className="flex gap-4 justify-center mt-8">
+            <Chip label="No plastic SIM cards" image="/no-sim.svg" />
+            <Chip label="No packaging" image="/delete.svg" />
+            <Chip label="No transport fees" image="/no_transfer.svg" />
+            <Chip label="No physical advertising" image="/scan.svg" />
+          </div>
         </div>
       </div>
     </LandingContainer>
