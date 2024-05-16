81e1b9a83483c6537d7e07a3b9f45669bcc5dabb
assilbekov <askhat.assilbekov@gmail.com>
2024-05-16 12:41:17 -0300

    adjust LandingSupport for xxxs screen

--- src/components/LandingSupport.tsx
+++ src/components/LandingSupport.tsx
@@ -3,22 +3,24 @@ import { LandingContainer } from "./LandingContainer";
 export const LandingSupport = () => {
   return (
     <LandingContainer>
-      <div className="w-full h-[444px] relative bg-[url('/support-background.png')] bg-[bottom_30%_right_55%] bg-cover rounded-xl">
+      <div className="w-full h-[208px] md:h-[444px] relative bg-[url('/support-background.png')] bg-[bottom_30%_right_55%] bg-cover rounded-xl">
         <div className="w-full h-full bg-gradient-to-br from-[rgba(55,18,0,0.2)] to-[rgba(55,18,0,0.00)] rounded-xl">
-          <div className="flex flex-col justify-between h-full w-1/2 pt-12 pb-14 pl-14">
-            <div className="flex flex-col gap-4">
-              <p className="text-text-900 font-medium text-2xl">Support</p>
-              <h3 className="text-text-900 text-5xl leading-[58px]">
+          <div className="flex flex-col justify-between h-full w-full md:w-1/2 p-6 md:pt-12 md:pb-14 md:pl-14">
+            <div className="flex flex-col gap-2 md:gap-4">
+              <p className="text-base leading-[22px] text-center md:text-2xl md:leading-[28px] text-text-900 font-medium">
+                Support
+              </p>
+              <h3 className="text-2xl leading-[30px] text-center font-medium md:text-5xl md:leading-[58px] text-text-900">
                 Reach out to us anytime, were available 24/7
               </h3>
             </div>
-            <div>
-              <button className="bg-secondary-500 py-4 px-6 rounded-full text-text-900 text-xl leading-7 font-medium">
+            <div className="text-center">
+              <button className="text-base leading-[22px] md:text-xl md:leading-7 py-4 px-8 md:px-6 bg-secondary-500 rounded-full text-text-900 font-medium">
                 Send a message
               </button>
             </div>
           </div>
-          <div className="bg-[url('/support-person.png')] bg-cover h-[478px] w-[340px] absolute right-[10%] bottom-0" />
+          <div className="hidden md:block bg-[url('/support-person.png')] bg-cover h-[478px] w-[340px] absolute right-[10%] bottom-0" />
         </div>
       </div>
     </LandingContainer>
