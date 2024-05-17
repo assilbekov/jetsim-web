5bd2de01ed5298366ac0c23a9275657be3414421
assilbekov <askhat.assilbekov@gmail.com>
2024-05-17 08:03:01 -0300

    add NoPlasticWaste xxxs screen

--- src/components/NoPlasticWaste.tsx
+++ src/components/NoPlasticWaste.tsx
@@ -8,11 +8,13 @@ type ChipProps = {
 
 const Chip = ({ label, image }: ChipProps) => {
   return (
-    <div className="flex gap-[10px] items-center bg-text-900 px-4 py-2 rounded-full">
-      <Image src={image} alt={label} width={24} height={24} />
-      <span className="text-primary-500 font-medium text-base leading-[22px]">
-        {label}
-      </span>
+    <div className="flex justify-center">
+      <div className="flex gap-[10px] items-center bg-text-900 px-4 py-2 rounded-full">
+        <Image src={image} alt={label} width={24} height={24} />
+        <span className="text-primary-500 font-medium text-base leading-[22px]">
+          {label}
+        </span>
+      </div>
     </div>
   );
 };
@@ -20,15 +22,15 @@ const Chip = ({ label, image }: ChipProps) => {
 export const NoPlasticWaste = () => {
   return (
     <LandingContainer>
-      <div className="w-full bg-[url('/no-waste-background.png')] bg-[bottom_50%_right_55%] bg-cover rounded-xl">
-        <div className="bg-gray-500 bg-opacity-50 py-11 px-20 rounded-xl">
-          <p className="text-text-900 font-medium text-xl leading-6 text-center">
+      <div className="w-full bg-[url('/no-waste-background.png')] bg-[bottom_50%_right_55%] bg-cover rounded-none md:rounded-xl">
+        <div className="p-6 pb-8 md:py-11 md:px-20 bg-gray-500 bg-opacity-50 rounded-none md:rounded-xl">
+          <p className="textbase leading-[22px] md:text-xl md:leading-6 text-text-900 font-medium text-center">
             No more plastic waste
           </p>
-          <h3 className="text-text-900 text-[34px] font-medium leading-[38px] text-center mt-2">
+          <h3 className="text-2xl leading-[30px] mt-1 md:mt-2 md:text-[34px] md:leading-[38px] text-text-900 font-medium text-center">
             Join us in our mission to save the planet
           </h3>
-          <div className="flex flex-col md:flex-row gap-4 md:justify-center mt-8">
+          <div className="flex flex-col md:flex-row gap-4 md:justify-center mt-4 md:mt-8">
             <Chip label="No plastic SIM cards" image="/no-sim.svg" />
             <Chip label="No packaging" image="/delete.svg" />
             <Chip label="No transport fees" image="/no_transfer.svg" />
