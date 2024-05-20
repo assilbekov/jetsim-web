8848d022adaca2d0dbd336ba0af9cc686a096c48
assilbekov <askhat.assilbekov@gmail.com>
2024-05-20 11:35:03 -0300

    add humburger menu small screen

--- src/components/LoginLink.tsx
+++ src/components/LoginLink.tsx
@@ -1,21 +1,16 @@
 import { useTranslations } from "next-intl";
-import Image from "next/image";
 import Link from "next/link";
 
 export const LoginLink = () => {
   const t = useTranslations("MainPage");
 
   return (
-    <div className="h-10 md:h-[52px] w-12 md:w-[107px] rounded-md md:rounded-[44px] border-2 bg-text-900 border-[#EBEFF0]">
-      <Link href="#" className="w-full h-full flex justify-center items-center text-text-100 font-medium text-base leading-[22px]">
-        <span className="md:block hidden">{t("login")}</span>
-        <Image
-          src="/login.svg"
-          alt="login icon"
-          width={20}
-          height={20}
-          className="md:hidden block"
-        />
+    <div className="h-[52px] w-full sm:w-[195px] md:w-[107px] rounded-[44px] border-2 bg-text-900 border-[#EBEFF0]">
+      <Link
+        href="#"
+        className="w-full h-full flex justify-center items-center text-text-100 font-medium text-base leading-[22px]"
+      >
+        <span>{t("login")}</span>
       </Link>
     </div>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,17 @@
 import { useTranslations } from "next-intl";
 import Image from "next/image";
-import Link from "next/link";
+import Link, { LinkProps } from "next/link";
 import { HumburgerButton } from "./HumburgerButton";
 
 import "./navbar.css";
 import { LoginLink } from "./LoginLink";
+
+const StyledLink = (props: LinkProps & { children: React.ReactNode }) => (
+  <Link
+    {...props}
+    className="text-xl font-medium leading-[26px] text-text-600"
+  />
+);
 
 export const Navbar = () => {
   const t = useTranslations("MainPage");
@@ -17,13 +24,13 @@ export const Navbar = () => {
       <nav
         id="primary-navigation"
         data-visible="false"
-        className="primary-navigation flex gap-10 text-text-600 md:w-2/3 md:justify-between"
+        className="primary-navigation flex gap-8 text-text-600 md:w-2/3 md:justify-between"
       >
-        <div className="flex flex-col md:flex-row gap-8 items-center">
-          <Link href="#">{t("check_compatability")}</Link>
-          <Link href="#">{t("how_to")}</Link>
-          <Link href="#">{t("support")}</Link>
-          <Link href="#">{t("faq")}</Link>
+        <div className="flex flex-col md:flex-row gap-6 md:gap-8 items-start md:items-center">
+          <StyledLink href="#">{t("check_compatability")}</StyledLink>
+          <StyledLink href="#">{t("how_to")}</StyledLink>
+          <StyledLink href="#">{t("support")}</StyledLink>
+          <StyledLink href="#">{t("faq")}</StyledLink>
         </div>
         <LoginLink />
       </nav>
