587af5f405d8d7a25417aee3ae714e75165640ef
assilbekov <askhat.assilbekov@gmail.com>
2024-05-21 09:54:59 -0300

    update NoPlasticWaste typo

--- src/components/NoPlasticWaste.tsx
+++ src/components/NoPlasticWaste.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image";
 import { LandingContainer } from "./LandingContainer";
+import { clsx } from "@/utils";
+import {
+  TypographyVariants,
+  getTypographyClass,
+  matchTypographyMediaQuery,
+} from "./Typography";
 
 type ChipProps = {
   label: string;
@@ -11,7 +17,12 @@ const Chip = ({ label, image }: ChipProps) => {
     <div className="flex justify-center">
       <div className="flex gap-[10px] items-center bg-text-900 px-4 py-2 rounded-full">
         <Image src={image} alt={label} width={24} height={24} />
-        <span className="text-primary-500 font-medium text-base leading-[22px]">
+        <span
+          className={clsx(
+            "text-primary-500",
+            getTypographyClass(TypographyVariants.Caption)
+          )}
+        >
           {label}
         </span>
       </div>
@@ -24,10 +35,26 @@ export const NoPlasticWaste = () => {
     <LandingContainer>
       <div className="w-full bg-[url('/no-waste-background.png')] bg-[bottom_50%_right_55%] bg-cover rounded-none sm:rounded-[20px]">
         <div className="p-6 pb-8 sm:px-8 sm:pt-8 md:py-11 md:px-20 bg-gray-500 bg-opacity-50 rounded-none sm:rounded-[20px]">
-          <p className="textbase leading-[22px] md:text-xl md:leading-6 text-text-900 font-medium text-center">
+          <p
+            className={clsx(
+              "text-text-900 text-center",
+              matchTypographyMediaQuery({
+                default: TypographyVariants.Caption,
+                md: TypographyVariants.Body,
+              })
+            )}
+          >
             No more plastic waste
           </p>
-          <h3 className="text-2xl leading-[30px] mt-1 md:mt-2 md:text-[34px] md:leading-[38px] text-text-900 font-medium text-center">
+          <h3
+            className={clsx(
+              "mt-1 md:mt-2 text-text-900 text-center",
+              matchTypographyMediaQuery({
+                default: TypographyVariants.Subheader,
+                md: TypographyVariants.H2,
+              })
+            )}
+          >
             Join us in our mission to save the planet
           </h3>
           <div className="flex flex-col sm:flex-row sm:flex-wrap gap-4 sm:justify-center mt-4 md:mt-8">
