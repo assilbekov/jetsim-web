c61e3bb8f8d3a16c6b4c60f9a49576bfc8f03df5
assilbekov <askhat.assilbekov@gmail.com>
2024-05-21 10:26:01 -0300

    add landing page typo

--- src/components/LandingSupport.tsx
+++ src/components/LandingSupport.tsx
@@ -1,21 +1,50 @@
+import { clsx } from "@/utils";
 import { LandingContainer } from "./LandingContainer";
+import {
+  TypographyVariants,
+  getTypographyClass,
+  matchTypographyMediaQuery,
+} from "./Typography";
 
 export const LandingSupport = () => {
   return (
     <LandingContainer>
       <div className="w-full h-[208px] sm:h-[272px] md:h-[276px] lg:h-[340px] xxl:h-[416px] relative bg-[url('/support-background.png')] bg-[bottom_30%_right_55%] bg-cover rounded-none sm:rounded-3xl md:rounded-xl">
         <div className="w-full h-full bg-gradient-to-br from-[rgba(55,18,0,0.2)] to-[rgba(55,18,0,0.00)] rounded-none sm:rounded-3xl md:rounded-xl">
-          <div className="flex flex-col justify-between h-full w-full sm:w-1/2 p-6 sm:p-8 sm:pr-4 md:py-6 lg:pt-12 md:pl-14 lg:pl-12 lg:pb-12">
+          <div className="flex flex-col justify-between h-full w-full sm:w-1/2 p-6 sm:p-8 sm:pr-0 md:py-6 lg:pt-12 md:pl-14 lg:pl-12 lg:pb-12">
             <div className="flex flex-col gap-2">
-              <p className="text-base leading-[22px] text-center sm:text-start lg:text-xl lg:leading-[26px] xxl:text-2xl xxl:leading-[28px] text-text-900 font-medium">
+              <p
+                className={clsx(
+                  "text-center sm:text-start text-text-900",
+                  matchTypographyMediaQuery({
+                    default: TypographyVariants.Caption,
+                    md: TypographyVariants.Body,
+                  }),
+                  "xxl:text-2xl xxl:leading-[30px]"
+                )}
+              >
                 Support
               </p>
-              <h3 className="text-2xl leading-[30px] text-center sm:text-start font-medium md:text-[34px] md:leading-[38px] lg:text-[34px] lg:leading-[38px] xxl:text-5xl xxl:leading-[58px] text-text-900">
+              <h3
+                className={clsx(
+                  "text-center sm:text-start text-text-900",
+                  matchTypographyMediaQuery({
+                    default: TypographyVariants.Subheader,
+                    md: TypographyVariants.H2,
+                  }),
+                  "xxl:font-interTight xxl:text-[56px] xxl:leading-[64px] xxl:tracking-[1.12px]"
+                )}
+              >
                 Reach out to us anytime, were available 24/7
               </h3>
             </div>
             <div className="text-center sm:text-start">
-              <button className="text-base leading-[22px] md:text-xl md:leading-7 py-4 px-8 md:px-6 bg-secondary-500 active:bg-secondary-300 hover:bg-secondary-700 rounded-full text-text-900 font-medium transition duration-200 ease-in-out">
+              <button
+                className={clsx(
+                  "py-4 px-8 md:px-6 bg-secondary-500 active:bg-secondary-300 hover:bg-secondary-700 rounded-full text-text-900 transition duration-200 ease-in-out",
+                  getTypographyClass(TypographyVariants.Caption)
+                )}
+              >
                 Send a message
               </button>
             </div>
