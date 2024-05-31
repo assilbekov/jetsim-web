3490233efffdef67aa53967f34caa29f5dd0c797
assilbekov <askhat.assilbekov@gmail.com>
2024-05-31 11:49:48 -0300

    add validateEmail func

--- src/components/LoginDialog/EmailLogin.tsx
+++ src/components/LoginDialog/EmailLogin.tsx
@@ -4,8 +4,21 @@ import { clsx } from "@/utils";
 import { TypographyVariants, getTypographyClass } from "../Typography";
 import { ChangeEmailButton } from "./ChangeEmailButton";
 import { SecondaryButton } from "./SecondaryButton";
+import { useMemo, useState } from "react";
+
+const validateEmail = (email: string): boolean => {
+  return Boolean(
+    String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      )
+  );
+};
 
 export const EmailLogin = () => {
+  const [email, setEmail] = useState("");
+  const isEmailValid = useMemo(() => validateEmail(email), [email]);
   return (
     <div className="w-full">
       <div className="relative">
@@ -15,6 +28,8 @@ export const EmailLogin = () => {
             "px-6 py-4 mt-6 whitespace-nowrap border-2 border-solid border-slate-200 rounded-[32px] w-full"
           )}
           placeholder="Enter email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
         <div className="absolute bottom-0 right-0 flex gap-4 items-center">
           <p
@@ -29,7 +44,9 @@ export const EmailLogin = () => {
         </div>
       </div>
 
-      <SecondaryButton disabled className="w-full">Continue with email</SecondaryButton>
+      <SecondaryButton disabled={!isEmailValid} className="w-full">
+        Continue with email
+      </SecondaryButton>
     </div>
   );
 };
