e0dec1f2142f10e60f6c99402ddfeac88c952890
assilbekov <askhat.assilbekov@gmail.com>
2024-06-04 11:35:58 -0300

    update div to p

--- src/components/InstallESim/ManualInstall.tsx
+++ src/components/InstallESim/ManualInstall.tsx
@@ -14,21 +14,21 @@ export const ManualInstall = ({ card }: ManualInstallProps) => {
   return (
     <div>
       <div className="flex flex-col p-6 mt-6 rounded-xl bg-slate-50 leading-[137.5%] text-slate-950">
-        <div>SM-DP+ Address</div>
-        <div className="mt-1 text-xl leading-7">{smdpAddr}</div>
+        <p>SM-DP+ Address</p>
+        <p className="mt-1 text-xl leading-7">{smdpAddr}</p>
         <CopyButton className="mt-4 w-full" text={smdpAddr} />
       </div>
       <div className="flex flex-col justify-center p-6 mt-4 rounded-xl bg-slate-50 leading-[137.5%] text-slate-950">
-        <div>Activation Code</div>
-        <div className="mt-1 text-xl leading-7">{lpaActivationCode}</div>
+        <p>Activation Code</p>
+        <p className="mt-1 text-xl leading-7">{lpaActivationCode}</p>
         <CopyButton className="mt-4 w-full" text={lpaActivationCode} />
       </div>
-      <div className="mt-6 text-2xl text-center text-black">
+      <p className="mt-6 text-2xl text-center text-black">
         Use this codes to add eSIM
-      </div>
-      <div className="mt-3 leading-6 text-center text-gray-400">
+      </p>
+      <p className="mt-3 leading-6 text-center text-gray-400">
         Copy this code when you manully adding eSIM
-      </div>
+      </p>
       <PrimaryButton className="mt-4 w-full">See instructions</PrimaryButton>
     </div>
   );
--- src/components/InstallESim/QRCodeInstall.tsx
+++ src/components/InstallESim/QRCodeInstall.tsx
@@ -13,18 +13,18 @@ export const QRCodeInstall = ({}: QRCodeInstallProps) => {
         src="https://cdn.builder.io/api/v1/image/assets/TEMP/4efaec2b1691cb2bc56fc731f91839f75304acc32ac4c5cb5f5958abc423d248?apiKey=4d29b65bc77c4658ad8f90690919294c&"
         className="self-center w-full aspect-square fill-white max-w-[220px]"
       />
-      <div className="mt-6 w-full text-2xl leading-8 text-center text-black">
+      <p className="mt-6 w-full text-2xl leading-8 text-center text-black">
         Scan this QR code from other device
-      </div>
-      <div className="mt-3 w-full leading-6 text-center text-gray-400">
+      </p>
+      <p className="mt-3 w-full leading-6 text-center text-gray-400">
         Share this QR code to other phone or laptop and scan it from there
-      </div>
-      <div className="justify-center items-center px-8 py-3 mt-4 w-full text-white bg-orange-600 rounded-[32px]">
+      </p>
+      <p className="justify-center items-center px-8 py-3 mt-4 w-full text-white bg-orange-600 rounded-[32px]">
         Share a QR code
-      </div>
-      <div className="justify-center items-center px-8 py-3 mt-4 w-full bg-white border-2 border-solid border-slate-200 rounded-[32px] text-slate-950">
+      </p>
+      <p className="justify-center items-center px-8 py-3 mt-4 w-full bg-white border-2 border-solid border-slate-200 rounded-[32px] text-slate-950">
         See instructions
-      </div>
+      </p>
     </div>
   );
 };
