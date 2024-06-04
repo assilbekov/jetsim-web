406e585665b0a2c67ba3ddd2e7cecfeedfb49a3e
assilbekov <askhat.assilbekov@gmail.com>
2024-06-04 11:52:13 -0300

    add CopyCard

--- src/components/InstallESim/ManualInstall.tsx
+++ src/components/InstallESim/ManualInstall.tsx
@@ -1,6 +1,33 @@
 import { Card } from "@/models/Card";
 import { CopyButton } from "../buttons/CopyButton";
 import { PrimaryButton } from "../buttons/PrimaryButton";
+import { clsx } from "@/utils";
+import { TypographyVariants, getTypographyClass } from "../Typography";
+
+type CopyCardProps = {
+  text: string;
+  label: string;
+  className?: string;
+};
+
+const CopyCard = ({ text, label, className }: CopyCardProps) => {
+  return (
+    <div className={clsx("p-6 rounded-xl bg-slate-50", className ?? "")}>
+      <p
+        className={clsx(
+          getTypographyClass(TypographyVariants.Caption),
+          "text-text-600"
+        )}
+      >
+        {label}
+      </p>
+      <p className={clsx(getTypographyClass(TypographyVariants.Body), "mt-1")}>
+        {text}
+      </p>
+      <CopyButton className="mt-4 w-full items-center" text={text} />
+    </div>
+  );
+};
 
 type ManualInstallProps = {
   card: Card;
@@ -13,20 +40,26 @@ export const ManualInstall = ({ card }: ManualInstallProps) => {
 
   return (
     <div>
-      <div className="flex flex-col p-6 mt-6 rounded-xl bg-slate-50 leading-[137.5%] text-slate-950">
-        <p>SM-DP+ Address</p>
-        <p className="mt-1 text-xl leading-7">{smdpAddr}</p>
-        <CopyButton className="mt-4 w-full" text={smdpAddr} />
-      </div>
-      <div className="flex flex-col justify-center p-6 mt-4 rounded-xl bg-slate-50 leading-[137.5%] text-slate-950">
-        <p>Activation Code</p>
-        <p className="mt-1 text-xl leading-7">{lpaActivationCode}</p>
-        <CopyButton className="mt-4 w-full" text={lpaActivationCode} />
-      </div>
-      <p className="mt-6 text-2xl text-center text-black">
+      <CopyCard text={smdpAddr} label="SM-DP+ Address" className="mt-6" />
+      <CopyCard
+        text={lpaActivationCode}
+        label="Activation Code"
+        className="mt-4"
+      />
+      <p
+        className={clsx(
+          getTypographyClass(TypographyVariants.Subheader),
+          "mt-6 text-2xl text-center text-black"
+        )}
+      >
         Use this codes to add eSIM
       </p>
-      <p className="mt-3 leading-6 text-center text-gray-400">
+      <p
+        className={clsx(
+          getTypographyClass(TypographyVariants.Caption),
+          "mt-3 text-center text-text-600"
+        )}
+      >
         Copy this code when you manully adding eSIM
       </p>
       <PrimaryButton className="mt-4 w-full">See instructions</PrimaryButton>
--- src/components/InstallESim/QRCodeInstall.tsx
+++ src/components/InstallESim/QRCodeInstall.tsx
@@ -1,30 +1,39 @@
 import { Card } from "@/models/Card";
-import * as React from "react";
+import QRCode from "react-qr-code";
+import { PrimaryButton } from "../buttons/PrimaryButton";
+import { SecondaryButton } from "../buttons/SecondaryButton";
+import { clsx } from "@/utils";
+import { TypographyVariants, getTypographyClass } from "../Typography";
 
 type QRCodeInstallProps = {
   card: Card;
 };
 
-export const QRCodeInstall = ({}: QRCodeInstallProps) => {
+export const QRCodeInstall = ({ card }: QRCodeInstallProps) => {
   return (
     <div className="flex flex-col justify-center text-base font-medium leading-6">
-      <img
-        loading="lazy"
-        src="https://cdn.builder.io/api/v1/image/assets/TEMP/4efaec2b1691cb2bc56fc731f91839f75304acc32ac4c5cb5f5958abc423d248?apiKey=4d29b65bc77c4658ad8f90690919294c&"
+      <QRCode
+        value={card.lpaCode}
         className="self-center w-full aspect-square fill-white max-w-[220px]"
       />
-      <p className="mt-6 w-full text-2xl leading-8 text-center text-black">
+      <p
+        className={clsx(
+          getTypographyClass(TypographyVariants.Subheader),
+          "mt-6 text-center text-text-100"
+        )}
+      >
         Scan this QR code from other device
       </p>
-      <p className="mt-3 w-full leading-6 text-center text-gray-400">
+      <p
+        className={clsx(
+          getTypographyClass(TypographyVariants.Caption),
+          "mt-3 w-full text-text-600 text-center"
+        )}
+      >
         Share this QR code to other phone or laptop and scan it from there
       </p>
-      <p className="justify-center items-center px-8 py-3 mt-4 w-full text-white bg-orange-600 rounded-[32px]">
-        Share a QR code
-      </p>
-      <p className="justify-center items-center px-8 py-3 mt-4 w-full bg-white border-2 border-solid border-slate-200 rounded-[32px] text-slate-950">
-        See instructions
-      </p>
+      <PrimaryButton className="mt-4">Share a QR code</PrimaryButton>
+      <SecondaryButton className="mt-4">See instructions</SecondaryButton>
     </div>
   );
 };
