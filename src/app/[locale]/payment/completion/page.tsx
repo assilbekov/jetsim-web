19a7632d7a422f7121157ddbf175b28e65a09918
assilbekov <askhat.assilbekov@gmail.com>
2024-06-04 08:16:11 -0300

    add test cards page

--- src/app/[locale]/payment/completion/page.tsx
+++ src/app/[locale]/payment/completion/page.tsx
@@ -1,3 +1,52 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import QRCode from "react-qr-code";
+
+interface Cards {
+  data: Card[];
+}
+
+interface Card {
+  id: string;
+  status: string;
+  lpaCode: string;
+  activatedAt: string;
+  expiresAt: string;
+  trafficTotalBytes: number;
+  trafficRemainingBytes: number;
+}
+
 export default function Completion() {
-  return <h1>Thank you! 🎉</h1>;
+  const [cards, setCards] = useState<Card[]>([]);
+
+  useEffect(() => {
+    fetch("https://sim.jetsim.app/api/v1/cards", {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    }).then(async (r) => {
+      const res: Cards = await r.json();
+      console.log({ res, r, message: "https://sim.jetsim.app/api/v1/cards" });
+      setCards(res.data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1>Thank you! 🎉</h1>
+      {cards.map((card) => (
+        <div key={card.id}>
+          <h2>Card ID: {card.id}</h2>
+          <h3>Status: {card.status}</h3>
+          <h3>LPA Code: {card.lpaCode}</h3>
+          <h3>Activated At: {card.activatedAt}</h3>
+          <h3>Expires At: {card.expiresAt}</h3>
+          <h3>Traffic Total Bytes: {card.trafficTotalBytes}</h3>
+          <h3>Traffic Remaining Bytes: {card.trafficRemainingBytes}</h3>
+          <QRCode value={card.lpaCode} />
+        </div>
+      ))}
+    </div>
+  );
 }
