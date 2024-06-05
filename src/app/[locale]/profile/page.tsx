d1b00d743eef7c76450b863cbeca9628ec0db02c
assilbekov <askhat.assilbekov@gmail.com>
2024-06-05 16:03:18 -0300

    add profile page

---
+++ src/app/[locale]/profile/page.tsx
@@ -0,0 +1,32 @@
+import { FAQ } from "@/components/FAQ";
+import { LandingContainer } from "@/components/LandingContainer";
+import { LandingFooter } from "@/components/LandingFooter";
+import { LandingSupport } from "@/components/LandingSupport";
+import { Navbar } from "@/components/Navbar";
+import { Recommendations } from "@/components/Recommendations";
+import { useTranslations } from "next-intl";
+
+export default function Index({
+  params,
+}: {
+  params: { placeId: string; locale: string };
+}) {
+  const t = useTranslations("Index");
+  return (
+    <main className="bg-[#F8F9FB] bg-white-900 overflow-hidden sm:pb-11">
+      <div className="bg-text-900 sm:bg-[#F8F9FB] shadow-[0px_4px_12px_0px_rgba(0,0,0,0.04)] pb-4 md:pb-5">
+        <LandingContainer className="px-4 xxs:px-6">
+          <div className="flex flex-col gap-6 pt-2 md:gap-9 xxs:pt-4 md:pt-6">
+            <Navbar />
+          </div>
+        </LandingContainer>
+      </div>
+      <div className="flex flex-col bg-[#F8F9FB] gap-4 md:gap-6">
+        <LandingSupport />
+        <Recommendations />
+        <FAQ />
+        <LandingFooter />
+      </div>
+    </main>
+  );
+}
