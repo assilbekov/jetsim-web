81c0342ad1c5dcb988a744dcde1b9de133fb100f
assilbekov <askhat.assilbekov@gmail.com>
2024-06-17 19:53:58 -0300

    refactor EnterDetailsManuallyStep

---
+++ src/components/InstallESimInstructionsDialog/EnterDetailsManuallyStep.tsx
@@ -0,0 +1,50 @@
+import { ArrowRightImage } from "./ArrowRightImage";
+import { IconWithText } from "./IconWithText";
+import { InfoRow } from "./InfoRow";
+import { RoundedLabel } from "./RoundedLabel";
+
+type EnterDetailsManuallyStep = {
+  step: number;
+};
+
+export const EnterDetailsManuallyStep = ({
+  step,
+}: EnterDetailsManuallyStep) => {
+  return (
+    <div>
+      <RoundedLabel>{step}</RoundedLabel>
+      <div>
+        <InfoRow>
+          <span>Go to</span>
+          <IconWithText
+            src="/icons/settings.png"
+            alt="settings icon"
+            text="Settings"
+          />
+          <ArrowRightImage />
+        </InfoRow>
+        <InfoRow>
+          <IconWithText
+            src="/icons/mobile-service.svg"
+            alt="mobile service icon"
+            text="Mobile Service"
+          />
+          <ArrowRightImage />
+          <span>Add eSIM</span>
+          <ArrowRightImage />
+        </InfoRow>
+        <InfoRow>
+          <IconWithText
+            src="/icons/primary/qr.svg"
+            alt="QR code icon"
+            text="Use QR Code"
+          />
+          <ArrowRightImage />
+        </InfoRow>
+        <InfoRow>
+          <span>Enter Details Manually</span>
+        </InfoRow>
+      </div>
+    </div>
+  );
+};
