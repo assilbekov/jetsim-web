f5e801515c3c2995ae81865f7eae081ba3fd4081
assilbekov <askhat.assilbekov@gmail.com>
2024-06-17 19:50:41 -0300

    add TurnOnDataRoamingStep

--- src/components/InstallESimInstructionsDialog/ManualInstall.tsx
+++ src/components/InstallESimInstructionsDialog/ManualInstall.tsx
@@ -88,6 +88,53 @@ export const EnterDetailsManuallyStep = ({
   );
 };
 
+type TurnOnDataRoamingStep = {
+  step: number;
+};
+
+export const TurnOnDataRoamingStep = ({ step }: TurnOnDataRoamingStep) => {
+  return (
+    <div>
+      <RoundedLabel>{step}</RoundedLabel>
+      <div>
+        <InfoRow>
+          <span>Go to</span>
+          <IconWithText
+            src="/icons/settings.png"
+            alt="settings icon"
+            text="Settings"
+          />
+          <ArrowRightImage />
+        </InfoRow>
+        <InfoRow>
+          <IconWithText
+            src="/icons/mobile-service.svg"
+            alt="mobile service icon"
+            text="Mobile Service"
+          />
+          <ArrowRightImage />
+        </InfoRow>
+        <InfoRow>
+          <IconWithText
+            src="/icons/primary/qr.svg"
+            alt="qr icon"
+            text="Select installed eSIM"
+          />
+          <ArrowRightImage />
+        </InfoRow>
+        <InfoRow>
+          <span>Turn on</span>
+          <IconWithText
+            src="/icons/toggle.svg"
+            alt="toggle icon"
+            text="Data Roaming"
+          />
+        </InfoRow>
+      </div>
+    </div>
+  );
+};
+
 type ManualInstallProps = {
   card: Card;
 };
@@ -101,6 +148,7 @@ export function ManualInstall({ card }: ManualInstallProps) {
       )}
     >
       <EnterDetailsManuallyStep step={1} />
+      <TurnOnDataRoamingStep step={5} />
       <div>
         <RoundedLabel>2</RoundedLabel>
         <p>Enter the following data</p>
