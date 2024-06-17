3e607d8bf1bca2ba17dff1c4c26d9da3e19c7993
assilbekov <askhat.assilbekov@gmail.com>
2024-06-17 20:43:33 -0300

    refactor EnterFollowingDataStep

--- src/components/InstallESimInstructionsDialog/EnterFollowingDataStep.tsx
+++ src/components/InstallESimInstructionsDialog/EnterFollowingDataStep.tsx
@@ -3,6 +3,23 @@ import { CopyButton } from "../buttons/CopyButton";
 import { RoundedLabel } from "./RoundedLabel";
 import { InfoRow } from "./InfoRow";
 
+type CopyBlockProps = {
+  text: string;
+  label: string;
+};
+
+const CopyBlock = ({ text, label }: CopyBlockProps) => {
+  return (
+    <div className="flex flex-col gap-4 items-center p-4 bg-white rounded-xl">
+      <div>
+        <p className="text-text-600">{label}</p>
+        {text}
+      </div>
+      <CopyButton text={text} />
+    </div>
+  );
+};
+
 type EnterFollowingDataStepProps = {
   step: number;
   card: Card;
@@ -12,13 +29,18 @@ export const EnterFollowingDataStep = ({
   step,
   card,
 }: EnterFollowingDataStepProps) => {
+  const [smdp, addr, lpaCode] = card.lpaCode.split("$");
+  const smdpAddr = `${smdp}$${addr}`;
+  const lpaActivationCode = `$${lpaCode}`;
+
   return (
-    <div>
+    <div className="flex flex-col gap-4">
       <RoundedLabel>{step}</RoundedLabel>
       <InfoRow>
         <span>Enter the following data</span>
       </InfoRow>
-      <CopyButton text="LPA:1$consumer.rsp.global" />
+      <CopyBlock text={smdpAddr} label="SM-DP+ Address" />
+      <CopyBlock text={lpaActivationCode} label="Activation Code" />
     </div>
   );
 };
