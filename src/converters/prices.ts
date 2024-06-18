6f76ea3af5951b2bc3db6f594d88b7c3a173665d
assilbekov <askhat.assilbekov@gmail.com>
2024-06-18 12:49:20 -0300

    add convertPrice

--- src/converters/prices.ts
+++ src/converters/prices.ts
@@ -3,3 +3,7 @@ import getSymbolFromCurrency from "currency-symbol-map";
 export const convertCurrencyCodeToSymbol = (currencyCode: string) => {
   return getSymbolFromCurrency(currencyCode);
 };
+
+export const convertPrice = (price: number, currency: string) => {
+  return `${convertCurrencyCodeToSymbol(currency)}${price / 100}`;
+};
