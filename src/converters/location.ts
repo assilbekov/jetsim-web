9114f7ce322e4b51a5f49627a26dfc5b3d4ced0f
assilbekov <askhat.assilbekov@gmail.com>
2024-06-18 12:54:02 -0300

    fix convertLocationBestCost

--- src/converters/location.ts
+++ src/converters/location.ts
@@ -1,8 +1,8 @@
 import { Location } from "@/models/Location";
-import { convertCurrencyCodeToSymbol } from "./prices";
+import { convertPrice } from "./prices";
 
 export const convertLocationBestCost = (location: Location) => {
-  return `from ${convertCurrencyCodeToSymbol(
-    location.bestCost?.cost.currency || ""
-  )}${location.bestCost?.cost.price}/${location.bestCost?.period}`;
+  const price = location.bestCost?.cost.price || 0;
+  const currency = location.bestCost?.cost.currency || "";
+  return `from ${convertPrice(price, currency)}/${location.bestCost?.period}`;
 };
