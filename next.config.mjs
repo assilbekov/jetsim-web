42225ab7439ebcef72b41dc7da09fa14d4166e6b
assilbekov <askhat.assilbekov@gmail.com>
2024-06-19 10:25:54 -0300

    delete next intl

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,12 @@
-import createNextIntlPlugin from "next-intl/plugin";
-
-const withNextIntl = createNextIntlPlugin();
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     formats: ["image/avif", "image/webp"],
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: '*',
-        port: '',
+        protocol: "https",
+        hostname: "*",
+        port: "",
       },
     ],
   },
