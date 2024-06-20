ed7a66c38d5d23e8f1d70b0ad44496f444925f01
assilbekov <askhat.assilbekov@gmail.com>
2024-06-20 14:57:01 -0300

    add phone texts

--- src/components/CheckCompatibility/CheckCompatibilityDialog.tsx
+++ src/components/CheckCompatibility/CheckCompatibilityDialog.tsx
@@ -117,18 +117,99 @@ export const CheckCompatibilityDialog = ({
           />
 
           <Paragraph>
-            Your device must be unlocked and the iOS version must be updated
-            to 14.1 or newer. You can check with your carrier to see if there
-            is anything you can do to unlock the eSIM in your device.
-          </Paragraph>
-
-          <Paragraph>
             Your device must be unlocked and the iOS version must be updated to
             14.1 or newer. You can check with your carrier to see if there is
             anything you can do to unlock the eSIM in your device.
           </Paragraph>
+
+          <Paragraph>
+            If you have a Turkish-produced device refer to{" "}
+            <LinkText href="https://support.apple.com/en-us/109343">
+              this instructions
+            </LinkText>
+          </Paragraph>
         </div>
       </AccordionPanel>
+
+      <AccordionPanel title="Android" index={1}>
+        <div className="w-full flex flex-col gap-4">
+          <Paragraph>
+            Your device must be unlocked. You can check with your carrier to see
+            if there is anything you can do to unlock the eSIM in your device.
+            Within one device model there can be devices both with eSIM
+            technology and without. Please check your device before the
+            purchase.
+          </Paragraph>
+
+          <ListBlock
+            title="Samsung"
+            elements={[
+              "Galaxy A55",
+              "Galaxy A54",
+              "Galaxy A35",
+              "Galaxy S24, S24+, S24 Ultra",
+              "Galaxy S23, S23+, S23 Ultra, S23 FE",
+              "Galaxy S22 5G, S22+ 5G, S22 Ultra 5G",
+              "Galaxy S21 5G, S21+ 5G, S21 Ultra 5G (US versions of S21 are not compatible with eSIM)",
+              "Galaxy S20, S20 5G, S20+, S20+ 5G, S20 Ultra, S20 Ultra 5G (US versions of S20 and S20 FE 4G/5G are not compatible with eSIM)",
+              "Galaxy Note20, Note20 5G, Note20 Ultra 5G (US and Hong Kong versions of Note 20 Ultra are not compatible with eSIM)",
+              "Galaxy Xcover7",
+              "Galaxy Fold",
+              "Galaxy Z Fold4",
+              "Galaxy Z Fold3 5G",
+              "Galaxy Z Fold2 5G",
+              "Galaxy Z Flip4",
+              "Galaxy Z Flip3 5G",
+              "Galaxy Z Flip and Z Flip 5G (US versions of Z Flip 5G are not compatible with eSIM)",
+            ]}
+          />
+
+          <ListBlock
+            title="Google Pixel"
+            elements={[
+              "Pixel 7, 7 Pro",
+              "Pixel 6, 6a, 6 Pro",
+              "Pixel 5, 5a 5G",
+              "Pixel 4, 4a, 4 XL, 4a 5G",
+              "Pixel 3, 3a*, 3 XL, 3a XL",
+            ]}
+            postText={
+              <>
+                Visit{" "}
+                <LinkText href="https://support.google.com/pixelphone/answer/9449293?hl=en">
+                  Google website
+                </LinkText>{" "}
+                to learn more about eSIM
+              </>
+            }
+          />
+
+          <ListBlock
+            title="Xiaomi"
+            elements={[
+              "Xiaomi 12T Pro",
+              "Xiaomi 13 Pro",
+              "Xiaomi 13",
+              "Xiaomi 13 Lite",
+              "Xiaomi 13T",
+              "Xiaomi 13T Pro",
+              "Redmi Note 13 Pro 5G",
+              "Redmi Note 13 Pro+ 5G",
+              "Xiaomi 14",
+            ]}
+            postText={
+              <>
+                Visit{" "}
+                <LinkText href="https://www.mi.com/global/support/esim/">
+                  Xiaomi website
+                </LinkText>{" "}
+                to learn more about eSIM
+              </>
+            }
+          />
+        </div>
+      </AccordionPanel>
+
       <AccordionPanel title="Other" index={2}>
         <div className="flex flex-col gap-4">
           <Paragraph>
