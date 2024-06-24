286cb12111c28371f71bc0c4bf31c76298c4089f
assilbekov <askhat.assilbekov@gmail.com>
2024-06-24 10:50:49 -0300

    add mmock cards

--- src/components/ProfileCards/index.tsx
+++ src/components/ProfileCards/index.tsx
@@ -10,13 +10,114 @@ import { useState } from "react";
 import { CardDialog, CardDialogModel, CardDialogType } from "./CardDialog";
 import { fetchPackage } from "@/api/packages";
 import { SkeletonCard } from "./SkeletonCard";
+import { CardStatus } from "@/models/Card";
+
+const MockCards = [
+  {
+    id: "6e9fc000-80fa-4d57-b458-d9127aeedf21",
+    status: CardStatus.ReadyToInstall,
+    lpaCode: "LPA:1$smdp.io$K2-1VL1UH-R5BLFY",
+    activatedAt: "2024-06-24 16:04:31 +0000 UTC",
+    expiresAt: "2024-07-09 16:04:31 +0000 UTC",
+    trafficTotalBytes: 37580963840,
+    trafficRemainingBytes: 37580963840,
+    package: {
+      id: "5ae1b16b-7b69-5903-8c4f-75b59a4a80da",
+      traffic: {
+        isUnlimited: true,
+      },
+    },
+    placeID: "mexico",
+  },
+  {
+    id: "debb0741-55bb-4713-967b-5434dbc31f34",
+    status: CardStatus.Installed,
+    lpaCode: "LPA:1$smdp.io$K2-1VL1UH-R5BLFY",
+    activatedAt: "2024-06-24 16:04:31 +0000 UTC",
+    expiresAt: "2024-07-09 16:04:31 +0000 UTC",
+    trafficTotalBytes: 37580963840,
+    trafficRemainingBytes: 37580963840,
+    package: {
+      id: "860a2144-35c2-5ec6-bcf6-7f491503f881",
+      traffic: {
+        isUnlimited: true,
+      },
+    },
+    placeID: "united-states",
+  },
+  {
+    id: "debb0741-55bb-4713-267b-5434dbc39f34",
+    status: CardStatus.Installed,
+    lpaCode: "LPA:1$smdp.io$K2-1VL1UH-R5BLFY",
+    activatedAt: "2024-06-24 16:04:31 +0000 UTC",
+    expiresAt: "2024-07-09 16:04:31 +0000 UTC",
+    trafficTotalBytes: 37580963840,
+    trafficRemainingBytes: 17580963840,
+    package: {
+      id: "ce1fd811-0cfe-5ba7-9433-2b276806c3e5",
+      traffic: {
+        isUnlimited: false,
+      },
+    },
+    placeID: "turkiye",
+  },
+  {
+    id: "debb0741-55bb-4713-957b-5434dbc39f34",
+    status: CardStatus.Expired,
+    lpaCode: "LPA:1$smdp.io$K2-1VL1UH-R5BLFY",
+    activatedAt: "2024-06-14 16:04:31 +0000 UTC",
+    expiresAt: "2024-06-19 16:04:31 +0000 UTC",
+    trafficTotalBytes: 27580963840,
+    trafficRemainingBytes: 37580963840,
+    package: {
+      id: "de0b2cdd-1e58-5f05-b471-34b609538b7d",
+      traffic: {
+        isUnlimited: false,
+      },
+    },
+    placeID: "albania",
+  },
+  {
+    id: "debb0741-55bb-4713-966b-5434dbc39f34",
+    status: CardStatus.Expired,
+    lpaCode: "LPA:1$smdp.io$K2-1VL1UH-R5BLFY",
+    activatedAt: "2024-06-14 16:04:31 +0000 UTC",
+    expiresAt: "2024-06-19 16:04:31 +0000 UTC",
+    trafficTotalBytes: 0,
+    trafficRemainingBytes: 37580963840,
+    package: {
+      id: "b59e0699-4090-52ed-85fe-8622f7acc3ef",
+      traffic: {
+        isUnlimited: false,
+      },
+    },
+    placeID: "united-kingdom",
+  },
+  {
+    id: "debb0741-25bb-4713-967b-5434dbc39f34",
+    status: CardStatus.Expired,
+    lpaCode: "LPA:1$smdp.io$K2-1VL1UH-R5BLFY",
+    activatedAt: "2024-06-14 16:04:31 +0000 UTC",
+    expiresAt: "2024-06-19 16:04:31 +0000 UTC",
+    trafficTotalBytes: 0,
+    trafficRemainingBytes: 37580963840,
+    package: {
+      id: "1b572a55-5435-5b24-8675-315eced6537c",
+      traffic: {
+        isUnlimited: false,
+      },
+    },
+    placeID: "united-arab-emirates",
+  },
+];
 
 export const ProfileCards = () => {
   const [dialog, setDialog] = useState<CardDialogModel | null>(null);
   const { data: cards, isFetched: isCardsFetched } = useQuery({
     queryKey: ["cards"],
     queryFn: async () => {
-      return await fetchCards();
+      const cardsRes = await fetchCards();
+      return MockCards;
     },
   });
 
