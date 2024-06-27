95a23b9b8dbac71c23ebca36a880fdc544ab5ab2
assilbekov <askhat.assilbekov@gmail.com>
2024-06-27 07:21:58 -0300

    add terms of service

--- src/app/terms-of-service/page.tsx
+++ src/app/terms-of-service/page.tsx
@@ -12,14 +12,77 @@ export default function Index() {
         </div>
         <div className="flex flex-col gap-4 md:gap-6 mt-4 sm:mt-6">
           <TermsTextBlock
-            title="Terms of service"
-            lastUpdated="2024-03-25"
+            title="User Agreement"
+            lastUpdated="June 20, 2024"
             infoBlocks={[
               {
-                header: "Introduction",
+                header: "General Provisions",
                 content: [
-                  "1.1 Terms. Thank you for choosing us. Before starting, please read these Terms and Conditions (“Terms”) carefully. Among other things, the Terms cover the scope of your use of and access to (i) Software (as described below) or website (“Website”) provided by Saily and (ii) eSIM with data service plans (“Data plans”) based on eSIM technology and other related services provided by our Technology Partner (as described below) and sold by Saily (collectively, the “Service” or “Services”).",
-                  "1.2 Updates to the Terms. Services might be introducing new features or modifying current ones constantly. In addition, we may need to update the Terms by amending them from time to time (i) to comply with the law; (ii) to comply with orders/recommendations of regulatory authorities; (iii) to add additional functionality, services, features, technologies and/or benefits, or to remove the old ones; (iv) to correct errors and bugs; (v) to clarify the Terms; (vi) to prevent abuse or harm; (vii) for any other valid reason (e.g., for security reasons). You understand and agree that it is your obligation to review the Terms from time to time in order to stay informed on current rules and obligations. Notification of any material changes to the Terms that affect your rights or obligations will be provided in advance of such changes by reasonable means (e.g., via email). Unless it is stated by us otherwise, each update of the Terms comes into force as of the moment when amended Terms are published. Your continued use of the Services will be deemed acceptance thereof. You can also end your relationship with us at any time after the effective date of the updated Terms by closing your Account and/or not accessing Websites and Services.",
+                  "1.1. These Terms and Conditions constitute a legally binding agreement (“Agreement”) made between you (the “User“) and GIBCO. LTD (hereinafter referred to as 'Jetsim,' 'we,' 'our'), concerning your purchase of an international data plan relating to a prepaid eSIM, for use on your personal device), from Jetsim.",
+                  "1.2. Jetsim shall make available Data Plans which relate to specific countries. Jetsim reserves the right to restrict the availability of such Data Plans in some jurisdictions.",
+                  "1.3. By availing of the Service, the User acknowledges that they have an unlocked, eSIM-compatible Device, which is required in order to access the eSIM.",
+                  "1.4. By registering on the Website or purchasing the Products, the User acknowledges their agreement with the terms outlined in this Agreement.",
+                  "1.5. The Website Administration reserves the right to amend this Agreement without prior notification to Users. The updated version of the Agreement comes into effect from the moment it is published on the Website.",
+                  "1.6. The User’s access to and use of the Website must comply with Google's Acceptable Use Policy and all applicable local, state, national, and international laws and regulations.",
+                ],
+              },
+              {
+                header: "User Registration",
+                content: [
+                  "2.1. To purchase Products on the Website, the User must complete the registration process, providing accurate and complete information about themselves.",
+                  "2.2. The User is responsible for the accuracy of the information provided and must promptly update it in the event of any changes.",
+                  "2.3. The User agrees not to use false identities or impersonate any other person or entity or falsely state or otherwise misrepresent their affiliation with a person or entity.",
+                ],
+              },
+              {
+                header: "Product Purchase Procedure",
+                content: [
+                  "3.1. To place an order, the User selects the desired Product on the Website and completes the order form.",
+                  "3.2. Payment for the Products must be made in accordance with the terms stated on the Website.",
+                ],
+              },
+              {
+                header: "Rights and Obligations of the Parties",
+                content: [
+                  "4.1. The User has the right to:",
+                  "4.1.1. Use the Website in accordance with the terms of this Agreement.",
+                  "4.1.2. Purchase the Products presented on the Website.",
+                  "4.1.3. Contact the Website's customer support service for any questions regarding the use of the Website and the purchase of Products.",
+                  "4.2. The User is obliged to:",
+                  "4.2.1. Not use the Website for illegal purposes.",
+                  "4.2.2. Not take any actions aimed at destabilizing the operation of the Website.",
+                  "4.2.3. Comply with the applicable laws of their jurisdiction when using the Website and purchasing the Products.",
+                  "4.2.4. Not engage in any activity that could harm the performance or functionality of any service or server, or the networks connected to any service or server.",
+                  "4.3. The Website Administration has the right to:",
+                  "4.3.1. Restrict User access to the Website in case of violation of this Agreement.",
+                  "4.3.2. Modify or remove any information provided by the User if it violates the laws of their jurisdiction or the rights of third parties.",
+                  "4.3.3. Gather necessary information about the User, in accordance with the Privacy Policy, for the purposes of providing services and for the security of both the User and Website.",
+                ],
+              },
+              {
+                header: "Liability",
+                content: [
+                  "5.1. The Website Administration is not liable for any damage caused to the User as a result of using or being unable to use the Website.",
+                  "5.2. The User is responsible for the accuracy of the information provided during registration and order placement.",
+                  "5.3. In case of violation of the terms of this Agreement, the Website Administration has the right to take any corresponding measures in accordance with the applicable laws of the User's jurisdiction.",
+                ],
+              },
+              {
+                header: "Property Rights",
+                content: [
+                  "6.1. Except as otherwise indicated, the eSIM is our proprietary property and all source code, databases, functionality, software, website designs, audio, video, text, photographs, and graphics on the Application (collectively, the “Content”) and the trade marks, service marks, and logos contained therein (the “Marks”) are owned or controlled by us or licensed to us, and are protected by copyright and trademark laws and various other intellectual property rights.",
+                  "6.2. Except as expressly provided in these Terms and Conditions, no part of the eSIM, no Content or Marks may be copied, reproduced, aggregated, republished, uploaded, posted, publicly displayed, encoded, translated, transmitted, distributed, sold, licensed, or otherwise exploited for any commercial purpose whatsoever, without our express prior written permission.",
+                ],
+              },
+              {
+                header: "Final Provisions",
+                content: [
+                  "7.1. This Agreement is governed by the laws of the jurisdiction in which the Website operates, unless otherwise stated.",
+                  "7.2. The Service are not intended for use by any person or entity in any jurisdiction or country where such use would be contrary to relevant law or regulation. Accordingly, persons who choose to access the Service from locations that prohibit such use do so on their own initiative and are solely responsible for compliance with local laws, if and to the extent local laws are applicable.",
+                  "7.3. All disputes and disagreements arising in connection with the execution of this Agreement shall be resolved through negotiations. If the parties fail to reach an agreement, the disputes shall be resolved in accordance with the applicable laws of the jurisdiction in which the Website operates.",
+                  "7.4. If any provision of this Agreement is found to be invalid or unenforceable by a court of competent jurisdiction, the remaining provisions will continue in full force and effect.",
+                  "7.5. The User acknowledges and agrees that Google's requirements, including but not limited to those found in their Acceptable Use Policy, must be complied with in all aspects of accessing and using the Website.",
+                  "7.6. By using the Website or purchasing Products, the User acknowledges that they have read, understood, and agreed to all terms and conditions stipulated in this Agreement.",
                 ],
               },
             ]}
