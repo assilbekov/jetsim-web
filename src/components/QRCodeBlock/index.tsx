df76a80719eaa9b5f7db84954e76365627fbaacf
assilbekov <askhat.assilbekov@gmail.com>
2024-06-28 16:52:05 -0300

    add title and desc

--- src/components/QRCodeBlock/index.tsx
+++ src/components/QRCodeBlock/index.tsx
@@ -1,18 +1,48 @@
 "use client";
 
 import { useDeviceTypeAndVerion } from "@/hooks/useDeviceTypeAndVerion";
+import { Card } from "@/models/Card";
+import { clsx } from "@/utils";
 import Image from "next/image";
 import QRCode from "qrcode.react";
+import { TypographyVariants, getTypographyClass } from "../Typography";
 
 type QRCodeBlockProps = {
-  value: string;
   size: number;
+  card: Card;
 };
 
-export const QRCodeBlock = ({ value, size }: QRCodeBlockProps) => {
+const Title = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <p
+      className={clsx(
+        getTypographyClass(TypographyVariants.Subheader),
+        "md:font-interTight md:text-[34px] md:leading-[38px] md:font-medium md:tracking-[0.68px]"
+      )}
+    >
+      {children}
+    </p>
+  );
+};
+
+const Description = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <p
+      className={clsx(
+        getTypographyClass(TypographyVariants.Body2),
+        "md:font-inter md:text-xl md:leading-[26px] md:font-medium",
+        "text-text-600"
+      )}
+    >
+      {children}
+    </p>
+  );
+};
+
+export const QRCodeBlock = ({ card, size }: QRCodeBlockProps) => {
   const deviceTypeAndVerion = useDeviceTypeAndVerion();
 
-  const url = `https://esimsetup.apple.com/esim_qrcode_provisioning?carddata=${value}`;
+  const url = `https://esimsetup.apple.com/esim_qrcode_provisioning?carddata=${card.lpaCode}`;
 
   const renderQRCode = () => {
     if (deviceTypeAndVerion.isIOS && deviceTypeAndVerion.version >= "17.5") {
@@ -36,10 +66,10 @@ export const QRCodeBlock = ({ value, size }: QRCodeBlockProps) => {
             height={52}
             width={37}
           />
-          <p>Press and hold on a QR code and select Add eSIM</p>
-          <p>
+          <Title>Press and hold on a QR code and select Add eSIM</Title>
+          <Description>
             Share this QR code to other phone or laptop and scan it from there
-          </p>
+          </Description>
         </div>
       );
     }
@@ -48,10 +78,10 @@ export const QRCodeBlock = ({ value, size }: QRCodeBlockProps) => {
       return (
         <div>
           <QRCode value={url} size={size} />
-          <p>Scan this QR code from other device</p>
-          <p>
+          <Title>Scan this QR code from other device</Title>
+          <Description>
             Share this QR code to other phone or laptop and scan it from there
-          </p>
+          </Description>
         </div>
       );
     }
@@ -60,8 +90,8 @@ export const QRCodeBlock = ({ value, size }: QRCodeBlockProps) => {
       return (
         <div>
           <QRCode value={url} size={size} />
-          <p>Install eSIM with QR code</p>
-          <p>Scan this QR code with your device</p>
+          <Title>Install eSIM with QR code</Title>
+          <Description>Scan this QR code with your device</Description>
         </div>
       );
     }
@@ -69,10 +99,10 @@ export const QRCodeBlock = ({ value, size }: QRCodeBlockProps) => {
     return (
       <div>
         <QRCode value={url} size={size} />
-        <p>Scan this QR code from other device</p>
-        <p>
+        <Title>Scan this QR code from other device</Title>
+        <Description>
           Share this QR code to other phone or laptop and scan it from there
-        </p>
+        </Description>
       </div>
     );
   };
