0afcbfb1f5e1b6df90382f7d161969ef936330b4
assilbekov <askhat.assilbekov@gmail.com>
2024-06-28 16:59:12 -0300

    update ScanQRStep

--- src/components/InstallESimInstructionsDialog/ScanQRStep.tsx
+++ src/components/InstallESimInstructionsDialog/ScanQRStep.tsx
@@ -5,6 +5,7 @@ import { InfoRow } from "./InfoRow";
 import { RoundedLabel } from "./RoundedLabel";
 import { Card } from "@/models/Card";
 import {} from "react-device-detect";
+import { useDeviceTypeAndVerion } from "@/hooks/useDeviceTypeAndVerion";
 
 type ScanQRStepProps = {
   step: number;
@@ -12,17 +13,26 @@ type ScanQRStepProps = {
 };
 
 export const ScanQRStep = ({ step, card }: ScanQRStepProps) => {
+  const deviceTypeAndVerion = useDeviceTypeAndVerion();
   const url = `https://esimsetup.apple.com/esim_qrcode_provisioning?carddata=${card.lpaCode}`;
   return (
     <div className="flex flex-col gap-5">
       <RoundedLabel>{step}</RoundedLabel>
-      <a href={url}>
-        <QRCode
+      {deviceTypeAndVerion.isIOS && deviceTypeAndVerion.version >= "17.5" ? (
+        <a href={url}>
+          <QRCode
+            value={url}
+            size={140}
+            className="self-center w-full aspect-square fill-white max-w-[140px]"
+          />
+        </a>
+      ) : (
+        <QRCodeCanvas
           value={url}
           size={140}
-          className="self-center w-full aspect-square fill-white max-w-[140px] max-h-[140px]"
+          className="self-center w-full aspect-square fill-white max-w-[140px]"
         />
-      </a>
+      )}
       <InfoRow>Scan this QR code</InfoRow>
     </div>
   );
