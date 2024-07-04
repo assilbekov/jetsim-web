48977427835bb2d4a3375d775e45405a16a9d88d
assilbekov <askhat.assilbekov@gmail.com>
2024-07-04 13:02:10 -0300

    add gta tags handling

--- src/components/anylitics-scripts/GA4.tsx
+++ src/components/anylitics-scripts/GA4.tsx
@@ -1,13 +1,37 @@
+"use client";
+
+import Script from "next/script";
+import { useEffect } from "react";
+
 const GA4_ID = "G-TG5HZCCKZE"; // Replace with your Google Analytics Measurement ID
 
 export const GA4 = () => {
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      (window as any).gtag("event", "page_view", {
+        page_path: url,
+      });
+    };
+
+    const handleRouteChangeComplete = () => {
+      handleRouteChange(window.location.pathname + window.location.search);
+    };
+
+    window.addEventListener("popstate", handleRouteChangeComplete);
+    handleRouteChangeComplete();
+
+    return () => {
+      window.removeEventListener("popstate", handleRouteChangeComplete);
+    };
+  }, []);
+
   return (
     <>
-      <script
+      <Script
         async
         src={`https://www.googletagmanager.com/gtag/js?id=${GA4_ID}`}
       />
-      <script
+      <Script
         dangerouslySetInnerHTML={{
           __html: `
             window.dataLayer = window.dataLayer || [];
