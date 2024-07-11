5a3a1a571d719e36cf710e0a8232de2d21132524
assilbekov <askhat.assilbekov@gmail.com>
2024-07-11 10:22:29 -0300

    add fb scripts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,10 @@ import { clsx } from "@/utils";
 import { ZendeskProvider } from "@/contexts/ZendeskProvider";
 import { GA4 } from "@/components/anylitics-scripts/GA4";
 import { GTM, GTMNoScript } from "@/components/anylitics-scripts/GTM";
+import {
+  Facebook,
+  FacebookNoScript,
+} from "@/components/anylitics-scripts/Facebook";
 
 const interTight = Inter_Tight({
   subsets: ["latin"],
@@ -24,9 +28,11 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <GA4 />
         <GTM />
+        <Facebook />
         <meta name="apple-mobile-web-app-capable" content="yes" />
       </head>
       <body>
+        <FacebookNoScript />
         <GTMNoScript />
         <QueryContext>
           <ZendeskProvider>{children}</ZendeskProvider>
