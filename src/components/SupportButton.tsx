ce9719866b1321865bb733dd39823f544a5424d0
assilbekov <askhat.assilbekov@gmail.com>
2024-07-16 15:29:10 -0300

    update SupportButton

--- src/components/SupportButton.tsx
+++ src/components/SupportButton.tsx
@@ -7,30 +7,9 @@ export const SupportButton = (props: {
   children: React.ReactNode;
   className?: string;
 }) => {
-  const isZendeskLoaded = useContext(ZendeskContext);
-  // ignore types for now.
-  const localWindow: any = typeof window !== "undefined" ? window : null;
-
-  const handleSupportClick = () => {
-    try {
-      const launcher = document.getElementById('launcher');
-      
-      if (launcher) {
-        launcher.style.display = 'block';
-      }
-
-      if (isZendeskLoaded && localWindow.zE) {
-        localWindow.zE("messenger", "open");
-      } else {
-        console.error("Zendesk Widget is not loaded");
-      }
-    } catch (error) {
-      console.error("Error opening Zendesk Widget:", error);
-    }
-  };
-
+  const {handleButtonClick} = useContext(ZendeskContext);
   return (
-    <div onClick={handleSupportClick} className={props.className}>
+    <div onClick={handleButtonClick} className={props.className}>
       {props.children}
     </div>
   );
--- src/contexts/ZendeskProvider.tsx
+++ src/contexts/ZendeskProvider.tsx
@@ -1,17 +1,53 @@
 "use client";
 
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState } from "react";
 
-export const ZendeskContext = createContext(false);
+type ZendeskContextType = {
+  isChatLoaded: boolean;
+  handleButtonClick: () => void;
+};
+
+export const ZendeskContext = createContext<ZendeskContextType>({
+  isChatLoaded: false,
+  handleButtonClick: () => {},
+});
 
 type ZendeskProviderProps = {
   children: React.ReactNode;
 };
 
 export function ZendeskProvider({ children }: ZendeskProviderProps) {
-  const [isZendeskLoaded, setIsZendeskLoaded] = useState(false);
+  const [isChatLoaded, setIsChatLoaded] = useState(false);
 
-  useEffect(() => {
+  const loadZendeskScript = () => {
+    const script = document.createElement("script");
+    script.id = "ze-snippet";
+    script.src =
+      "https://static.zdassets.com/ekr/snippet.js?key=5c4d404b-f742-403a-bd86-b941352f96fc";
+    script.async = true;
+    script.onload = () => {
+      console.log("Zendesk Widget loaded");
+      const localWindow: any = typeof window !== "undefined" ? window : null;
+      setIsChatLoaded(true);
+      localWindow.zE("messenger", "open");
+    };
+    script.onerror = () => {
+      console.error("Failed to load Zendesk Widget script");
+    };
+    document.head.appendChild(script);
+  };
+
+  const handleButtonClick = () => {
+    if (!isChatLoaded) {
+      loadZendeskScript();
+    } else {
+      const localWindow: any = typeof window !== "undefined" ? window : null;
+      localWindow.zE("messenger", "open");
+      localWindow.zE("webWidget", "show");
+    }
+  };
+
+  /* useEffect(() => {
     const script = document.createElement("script");
     script.id = "ze-snippet";
     script.src =
@@ -25,10 +61,15 @@ export function ZendeskProvider({ children }: ZendeskProviderProps) {
       console.error("Failed to load Zendesk Widget script");
     };
     document.head.appendChild(script);
-  }, []);
+  }, []); */
 
   return (
-    <ZendeskContext.Provider value={isZendeskLoaded}>
+    <ZendeskContext.Provider
+      value={{
+        isChatLoaded,
+        handleButtonClick,
+      }}
+    >
       {children}
     </ZendeskContext.Provider>
   );
