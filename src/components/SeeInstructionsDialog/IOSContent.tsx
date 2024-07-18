ae9e83f11004bbea893b473259065437bd5c2e85
assilbekov <askhat.assilbekov@gmail.com>
2024-07-18 17:38:25 -0300

    finish IOSContent

--- src/components/SeeInstructionsDialog/IOSContent.tsx
+++ src/components/SeeInstructionsDialog/IOSContent.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Card } from "@/models/Card";
 import { clsx } from "@/utils";
 import { TypographyVariants, getTypographyClass } from "../Typography";
@@ -7,12 +9,44 @@ import { TurnOnDataRoamingStep } from "../InstallESimInstructionsDialog/TurnOnDa
 import { StyledContent } from "./components/StyledContent";
 import { EnterDetailsManuallyStep } from "./components/EnterDetailsManuallyStep";
 import { EnterFollowingDataStep } from "./components/EnterFollowingDataStep";
+import { useDeviceTypeAndVerion } from "@/hooks/useDeviceTypeAndVerion";
+import { UseQRDetailsStep } from "./components/UseQRDetailsStep";
 
 type IOSContentProps = {
   card: Card;
 };
 
 export const IOSContent = ({ card }: IOSContentProps) => {
+  const deviceTypeAndVerion = useDeviceTypeAndVerion();
+
+  const renderQRInstallation = () => {
+    if (deviceTypeAndVerion.isIOS && deviceTypeAndVerion.version >= "16") {
+      return (
+        <StyledContent>
+          <ScanQRStep step={1} card={card} />
+          <PlanTextStep step={2}>
+            Follow screen instructions to install eSIM
+          </PlanTextStep>
+          <TurnOnDataRoamingStep step={3} />
+        </StyledContent>
+      );
+    }
+
+    return (
+      <StyledContent>
+        <ScanQRStep step={1} card={card} />
+        <UseQRDetailsStep step={2} />
+        <PlanTextStep step={3}>
+          Scan shared QR code from other screen with this phone
+        </PlanTextStep>
+        <PlanTextStep step={4}>
+          Follow screen instructions to install eSIM
+        </PlanTextStep>
+        <TurnOnDataRoamingStep step={5} />
+      </StyledContent>
+    );
+  };
+
   return (
     <div
       className={clsx(
@@ -20,13 +54,7 @@ export const IOSContent = ({ card }: IOSContentProps) => {
         "w-full text-center"
       )}
     >
-      <StyledContent>
-        <ScanQRStep step={1} card={card} />
-        <PlanTextStep step={2}>
-          Follow screen instructions to install eSIM
-        </PlanTextStep>
-        <TurnOnDataRoamingStep step={3} />
-      </StyledContent>
+      {renderQRInstallation()}
       <p className="mt-5 mb-4">Or use alternative option</p>
       <StyledContent>
         <EnterDetailsManuallyStep step={1} />
