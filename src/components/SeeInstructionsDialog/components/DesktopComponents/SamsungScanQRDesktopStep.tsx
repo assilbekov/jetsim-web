3d76613d7d2ea19e5c5522ac524f8909899555aa
assilbekov <askhat.assilbekov@gmail.com>
2024-07-22 10:11:33 -0300

    add SamsungScanQRDesktopStep component

---
+++ src/components/SeeInstructionsDialog/components/DesktopComponents/SamsungScanQRDesktopStep.tsx
@@ -0,0 +1,49 @@
+import { ArrowRightImage } from "../ArrowRightImage";
+import { IconWithText } from "../IconWithText";
+import { InfoRow } from "../InfoRow";
+import { RoundedLabel } from "../RoundedLabel";
+
+type SamsungScanQRDesktopStepProps = {
+  step: number;
+};
+
+export const SamsungScanQRDesktopStep = ({
+  step,
+}: SamsungScanQRDesktopStepProps) => {
+  return (
+    <div>
+      <RoundedLabel>{step}</RoundedLabel>
+      <div className="mt-4 flex flex-col gap-[10px]">
+        <InfoRow>
+          <span>Go to</span>
+          <IconWithText
+            src="/icons/settings-blue.png"
+            alt="settings icon"
+            text="Settings"
+          />
+          <ArrowRightImage />
+        </InfoRow>
+        <InfoRow>
+          <IconWithText
+            src="/icons/connection-blue.svg"
+            alt="wifi icon"
+            text="Connections"
+          />
+          <ArrowRightImage />
+          <span>SIM manager</span>
+          <ArrowRightImage />
+        </InfoRow>
+        <InfoRow>
+          <IconWithText
+            src="/icons/plus-green.svg"
+            alt="add icon"
+            text="Add eSIM"
+          />
+          <ArrowRightImage />
+          <span>Scan QR code</span>
+          <ArrowRightImage />
+        </InfoRow>
+      </div>
+    </div>
+  );
+};
