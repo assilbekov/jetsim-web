8881f413a057f7e67b71bd9ce2343e7c31c3bc4a
assilbekov <askhat.assilbekov@gmail.com>
2024-07-29 16:45:47 -0300

    pause

--- src/components/ProfileInviteFriends/index.tsx
+++ src/components/ProfileInviteFriends/index.tsx
@@ -1,19 +1,27 @@
 "use client";
 
 import Image from "next/image";
+import { LandingContainer } from "../LandingContainer";
+import { Card } from "../Card";
+
+type InfoBoxProps = {
+  label: React.ReactNode;
+  value: number;
+  valuePrefix?: string;
+}
 
 export const ProfileInviteFriends = () => {
   return (
-    <div>
-      <div>
+    <LandingContainer>
+      <Card className="p-6 pt-0 xxs:pt-0">
         <Image
           src="/images/invite-friends-bg.jpg"
           alt="Invite friends hero image"
           width={272}
           height={168}
-          className="w-full rounded-xl md:hidden mt-6"
+          className="w-full rounded-xl"
         />
-      </div>
-    </div>
+      </Card>
+    </LandingContainer>
   );
 };
