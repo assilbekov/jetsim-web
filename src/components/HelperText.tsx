d15fd1670c12d63298d13469f12db9cff022a3b0
assilbekov <askhat.assilbekov@gmail.com>
2024-07-30 13:26:24 -0300

    add InfoBox and helper text compoennts

---
+++ src/components/HelperText.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import Image from "next/image";
+import { TypographyVariants, getTypographyClass } from "./Typography";
+import { useState } from "react";
+
+type PlansHelperInfoProps = {
+  title: React.ReactNode;
+  description: React.ReactNode;
+};
+
+export const HelperText = ({ title, description }: PlansHelperInfoProps) => {
+  const [open, setOpen] = useState(false);
+
+  const handleToggle = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    event.stopPropagation();
+    setOpen(!open);
+  };
+
+  return (
+    <div className="relative">
+      <div
+        className="flex justify-center relative items-center min-w-6 min-h-6 before:absolute before:top-1/2 before:left-1/2 before:w-10 before:h-10 before:-translate-x-1/2 before:-translate-y-1/2"
+        onClick={handleToggle}
+      >
+        <Image
+          src="/icons/gray/info.svg"
+          alt="info icon"
+          width={16}
+          height={16}
+        />
+      </div>
+      {open && (
+        <>
+          <div className="absolute w-72 -right-4 top-12 p-4 pt-[14px] flex gap-2 items-start z-10 rounded-2xl border-[2px] border-[#E9F0F2] bg-white shadow-[0px_4px_12px_0px_rgba(0,0,0,0.04)]">
+            <div>
+              <h6 className={getTypographyClass(TypographyVariants.Caption)}>
+                {title}
+              </h6>
+              {description}
+            </div>
+            <div
+              className="flex justify-center relative items-center min-w-6 min-h-6 before:absolute before:top-1/2 before:left-1/2 before:w-10 before:h-10 before:-translate-x-1/2 before:-translate-y-1/2"
+              onClick={handleToggle}
+            >
+              <Image
+                src="/icons/gray/close.svg"
+                alt="close icon"
+                width={24}
+                height={24}
+                className="absolute w"
+              />
+            </div>
+          </div>
+          <div className="fixed inset-0" onClick={handleToggle} />
+        </>
+      )}
+    </div>
+  );
+};
--- src/components/ProfileInviteFriends/index.tsx
+++ src/components/ProfileInviteFriends/index.tsx
@@ -3,12 +3,42 @@
 import Image from "next/image";
 import { LandingContainer } from "../LandingContainer";
 import { Card } from "../Card";
+import { HelperText } from "../HelperText";
+import { clsx } from "@/utils";
 
 type InfoBoxProps = {
   label: React.ReactNode;
   value: number;
   valuePrefix?: string;
-}
+  helperText?: React.ReactNode;
+  rootClassName?: string;
+};
+
+const InfoBox = ({
+  label,
+  value,
+  valuePrefix,
+  helperText,
+  rootClassName,
+}: InfoBoxProps) => {
+  return (
+    <div
+      className={clsx(
+        "px-3 py-[10px] border-2 rounded-xl border-solid border-[#E9F0F2]",
+        rootClassName || ""
+      )}
+    >
+      <div className="flex justify-between">
+        <p>{label}</p>
+        {helperText && <HelperText title={label} description={helperText} />}
+      </div>
+      <p className={clsx(value ? "text-primary-500" : "text-text-100")}>
+        {valuePrefix || ""}
+        {value}
+      </p>
+    </div>
+  );
+};
 
 export const ProfileInviteFriends = () => {
   return (
@@ -21,6 +51,20 @@ export const ProfileInviteFriends = () => {
           height={168}
           className="w-full rounded-xl"
         />
+        <div className="grid grid-cols-2 xxs:grid-cols-3 gap-3">
+          <InfoBox label="Pending" value={0} valuePrefix="$" />
+          <InfoBox
+            label="Pending"
+            value={10}
+            valuePrefix="$"
+            helperText="It's how much you will earn if your friend buys a plan. Remind your friend to get their $5 reward to buy an eSIM with a discount!"
+          />
+          <InfoBox
+            label="Pending"
+            value={2}
+            rootClassName="col-span-2 xxs:col-span-1"
+          />
+        </div>
       </Card>
     </LandingContainer>
   );
