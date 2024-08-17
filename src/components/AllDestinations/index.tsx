c4607abb5d7a2abffce4c43d10105ab77956be5c
assilbekov <askhat.assilbekov@gmail.com>
2024-08-17 18:52:15 +0500

    add all destinations intl

--- src/components/AllDestinations/index.tsx
+++ src/components/AllDestinations/index.tsx
@@ -4,8 +4,10 @@ import { LandingContainer } from "../LandingContainer";
 import { TypographyVariants, getTypographyClass } from "../Typography";
 import { Search } from "../Search";
 import { AllDestinationsList } from "./AllDestinationsList";
+import { useTranslations } from "next-intl";
 
 export const AllDestinations = () => {
+  const t = useTranslations("AllDestinations");
   return (
     <LandingContainer>
       <Card className="xxxs:px-6 xxs:py-4 xs:py-6 lg:pt-12">
@@ -16,7 +18,7 @@ export const AllDestinations = () => {
               "md:font-interTight md:text-[56px] md:leading-[64px] md:font-medium md:tracking-[1.12px] text-center"
             )}
           >
-            All destinations
+            {t("title")}
           </h1>
           <h3
             className={clsx(
@@ -24,8 +26,7 @@ export const AllDestinations = () => {
               "md:text-2xl md:leading-[30px] text-center mt-4 mb-6 md:mb-9 text-text-600"
             )}
           >
-            Explore data plans across 150+ countries for seamless and secure
-            internet access on the go
+            {t("description")}
           </h3>
           <Search />
         </div>
