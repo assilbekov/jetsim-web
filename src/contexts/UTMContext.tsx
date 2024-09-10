a081d324f026a1d4f159e133eb72fc3bacdf0e29
assilbekov <askhat.assilbekov@gmail.com>
2024-09-10 21:17:43 +0500

    Add handleUTMCaptureEvent

--- src/contexts/UTMContext.tsx
+++ src/contexts/UTMContext.tsx
@@ -1,13 +1,14 @@
 "use client";
 
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { handleUTMCaptureEvent } from "@/gtm-events";
 
 type UTM = {
   utm_source: string;
   utm_medium: string;
   utm_campaign: string;
   utm_term: string;
-  //utm_content: string;
+  utm_content: string;
 };
 
 type UTMContextType = {
@@ -32,29 +33,30 @@ const getUtmParam = (urlParams: URLSearchParams, utmTag: string) => {
 };
 
 export const UTMProvider = ({ children }: { children: React.ReactNode }) => {
-  const [utms] = useState<UTM | null>(() => {
+  const [utms, setUtms] = useState<UTM | null>(null);
+
+  useEffect(() => {
     if (typeof window === "undefined") {
-      return null;
+      return;
     }
 
-    const urlParams = new URLSearchParams(window?.location.search);
-
-    return {
+    const urlParams = new URLSearchParams(window.location.search);
+    const newUtms = {
       utm_source: getUtmParam(urlParams, "utm_source"),
       utm_medium: getUtmParam(urlParams, "utm_medium"),
       utm_campaign: getUtmParam(urlParams, "utm_campaign"),
       utm_term: getUtmParam(urlParams, "utm_term"),
+      utm_content: getUtmParam(urlParams, "utm_content"),
     };
-  });
+    setUtms(newUtms);
+
+    // Send custom event to GA4 with UTM parameters
+    handleUTMCaptureEvent(newUtms);
+  }, []);
 
   const utmsSearchParams = utms
     ? Object.entries(utms)
-        .map(([k, v]) => {
-          if (v) {
-            return `${k}=${v}`;
-          }
-          return "";
-        })
+        .map(([k, v]) => (v ? `${k}=${v}` : ""))
         .filter(Boolean)
         .join("&")
     : "";
