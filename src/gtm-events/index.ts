6c988ee9963ea10dc697d58960133449267fde4c
assilbekov <askhat.assilbekov@gmail.com>
2024-09-11 12:43:26 +0500

    send registration event

--- src/gtm-events/index.ts
+++ src/gtm-events/index.ts
@@ -248,6 +248,14 @@ export const handlePaymentAttemptEvent = () => {
 
 export const handleRegistrationEvent = (data: any) => {
   handleGTMEvent("registration", data);
+  const allFields = { ...getDefaultFields(), ...data };
+  const localWindow: any = window;
+  localWindow.gtag("event", "registration", {
+    category: "Ecommerce",
+    label: "Registration",
+    //value: allFields,
+    ...allFields,
+  });
 };
 
 export const handleUTMCaptureEvent = (data: any) => {
