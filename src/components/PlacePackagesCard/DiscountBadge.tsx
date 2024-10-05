15ad41aae5c7f0761bbd3885dc81a3c3f8a13346
assilbekov <askhat.assilbekov@gmail.com>
2024-10-05 18:03:50 +0500

    Add discount badge color style

--- src/components/PlacePackagesCard/DiscountBadge.tsx
+++ src/components/PlacePackagesCard/DiscountBadge.tsx
@@ -7,13 +7,14 @@ type DiscountBadgeProps = {
 
 export const DiscountBadge = ({ discount }: DiscountBadgeProps) => {
   return (
-    <div className="relative h-[24px] w-[61px] bg-[url('/images/discount-bg.png')] bg-no-repeat bg-cover flex items-center justify-center  text-[#00B5F2]">
-      <Typography
-        variant={TypographyVariants.Caption}
-        className="font-semibold"
+    <div className="relative h-[24px] w-[61px] bg-[url('/images/discount-bg.png')] bg-no-repeat bg-cover flex items-center justify-center">
+      <span
+        className="font-semibold text-base leading-6 text-[#00B5F2]"
+        // Somewhy text-[#00B5F2] class doesn't work
+        style={{ color: "#00B5F2" }}
       >
         -{discount}%
-      </Typography>
+      </span>
     </div>
   );
 };
