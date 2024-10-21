adaf675fe5f6ff4991c14d3d7f224b81cec44169
assilbekov <askhat.assilbekov@gmail.com>
2024-10-21 19:32:25 +0500

    fix styling issues

--- src/components/StripePayment/TotalPrice.tsx
+++ src/components/StripePayment/TotalPrice.tsx
@@ -1,133 +1,14 @@
 "use client";
 
 import Odometer from "react-odometerjs";
-import { fetchLocation } from "@/api/locations";
-import { fetchPackage } from "@/api/packages";
-import { Location } from "@/models/Location";
-import { Package } from "@/models/Package";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { TypographyVariants, getTypographyClass } from "../Typography";
-import { CircledCountryImage } from "../CircledCountryImage";
 import { convertCurrencyCodeToSymbol, convertPrice } from "@/converters/prices";
-import { Skeleton } from "../Skeleton";
 import { Card } from "../Card";
 import { clsx } from "@/utils";
-import { convertDaysText } from "@/converters/texts";
 import { useTranslations } from "next-intl";
 import { ToggleSwitch } from "../inputs/ToggleSwitch";
 import "odometer/themes/odometer-theme-default.css";
-
-interface PricingProps {
-  originalPrices: number[];
-  discountedPrices: number[];
-}
-
-const Pricing: React.FC<PricingProps> = ({
-  originalPrices,
-  discountedPrices,
-}) => {
-  const [isDiscounted, setIsDiscounted] = useState(false);
-  const prices = isDiscounted ? discountedPrices : originalPrices;
-
-  return (
-    <div className="flex flex-col items-center">
-      {prices.map((price, index) => (
-        <Odometer
-          key={index}
-          value={price}
-          format="(.ddd),dd"
-          className="text-2xl"
-          duration={150}
-        />
-      ))}
-      <ToggleSwitch checked={isDiscounted} onChange={setIsDiscounted} />
-    </div>
-  );
-};
-
-// Define the price data type
-type PriceData = {
-  original: number;
-  discounted: number;
-};
-
-const PriceDisplay: React.FC = () => {
-  const [showDiscount, setShowDiscount] = useState(false);
-
-  const prices: PriceData[] = [
-    { original: 15.0, discounted: 12.0 },
-    { original: 18.5, discounted: 15.5 },
-    { original: 26.2, discounted: 22.2 },
-    { original: 54.1, discounted: 45.1 },
-  ];
-
-  const handleToggle = (checked: boolean) => {
-    setShowDiscount(checked);
-  };
-
-  return (
-    <div className="font-sans text-2xl">
-      <div className="space-y-4">
-        {prices.map((price, index) => (
-          <div key={index} className="price p-2 flex items-center">
-            <span className="currency mr-1">$</span>
-            <Odometer
-              value={showDiscount ? price.discounted : price.original}
-              format="(,ddd).dd"
-              duration={150}
-              theme="minimal"
-            />
-            <span className="unit ml-1"> / GB</span>
-          </div>
-        ))}
-      </div>
-      <div className="mt-4 flex items-center">
-        <ToggleSwitch checked={showDiscount} onChange={handleToggle} />
-        <span className="ml-2">
-          {showDiscount ? "Discounted" : "Original"} Prices
-        </span>
-      </div>
-    </div>
-  );
-};
-
-const OdometerComponent = () => {
-  const [value, setValue] = useState(1233244);
-
-  useEffect(() => {
-    const timeoutId = setTimeout(() => setValue(4323441), 10);
-    return () => {
-      clearTimeout(timeoutId);
-    };
-  }, []);
-
-  return (
-    <Odometer
-      value={value}
-      format="(.ddd),dd"
-      className="text-2xl"
-      duration={150}
-    />
-  );
-};
-
-type OrderElementProps = {
-  title: React.ReactNode;
-  value: React.ReactNode;
-  className?: string;
-};
-
-const OrderElement = ({ title, value }: OrderElementProps) => {
-  return (
-    <div className="font-inter text-sm leading-[18px] font-medium sm:text-[16px] sm:leading-[19px]">
-      <div className="shrink-0 mt-3 h-0.5 bg-[#E9F0F2]" />
-      <div className="flex gap-5 justify-between mt-3 whitespace-nowrap">
-        <p>{title}</p>
-        <p>{value}</p>
-      </div>
-    </div>
-  );
-};
 
 type TotalPriceProps = {
   totalPrice: number;
@@ -146,12 +27,6 @@ export const TotalPrice = ({
 }: TotalPriceProps) => {
   const t = useTranslations("OrderSummary");
   const [isRewardsUsed, setIsRewardsUsed] = useState(false);
-
-  /* if (!packageData || !location) {
-    return (
-      <Skeleton className="min-w-full min-h-[250px] rounded-[20px] sm:max-w-[300px] sm:min-w-[300px]" />
-    );
-  } */
 
   return (
     <Card
@@ -176,18 +51,38 @@ export const TotalPrice = ({
           />
         </span>
       </div>
-      <OrderElement
-        title={t("fullPrice")}
-        value={convertPrice(fullPrice, currency)}
-      />
-      <OrderElement
-        title={t("Rewards –30%")}
-        value={-convertPrice(rewardsBalance, currency)}
-      />
-      <div>
+      <div
+        className={clsx(
+          "flex justify-between items-center text-[#95A2A6] mt-2",
+          getTypographyClass(TypographyVariants.Body2)
+        )}
+      >
+        <span>Price</span>
+        <span>{convertPrice(fullPrice, currency)}</span>
+      </div>
+      <div
+        className={clsx(
+          "flex justify-between items-center text-[#95A2A6] mt-1",
+          getTypographyClass(TypographyVariants.Body2)
+        )}
+      >
+        <span>Rewards</span>
+        <span>{convertPrice(rewardsBalance, currency)}</span>
+      </div>
+      <div className="shrink-0 my-5 h-0.5 bg-[#E9F0F2]" />
+      <div className="flex justify-between items-center">
         <div>
-          <h6>Use rewards</h6>
-          <p>Balance $30</p>
+          <h6 className={getTypographyClass(TypographyVariants.Body)}>
+            Use rewards
+          </h6>
+          <p
+            className={clsx(
+              getTypographyClass(TypographyVariants.Body2),
+              "text-[#95A2A6] mt-1"
+            )}
+          >
+            Balance $30
+          </p>
         </div>
         <ToggleSwitch checked={isRewardsUsed} onChange={setIsRewardsUsed} />
       </div>
